2d3ecb47f181448de939b7ef62be4cdcadb4938f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-12 09:31:50 +0200

    feature: add searchFileWithRipGrep command (#8)

---
+++ src/parts/SearchFileWithRipGrep/SearchFileWithRipGrep.ts
@@ -0,0 +1,17 @@
+// TODO create direct connection from electron to file search worker using message ports
+
+import * as GetFileSearchRipGrepArgs from '../GetFileSearchRipGrepArgs/GetFileSearchRipGrepArgs.ts'
+import * as SearchProcess from '../SearchProcess/SearchProcess.ts'
+import * as SplitLines from '../SplitLines/SplitLines.ts'
+
+export const searchFile = async (path: string, value: string) => {
+  const ripGrepArgs = GetFileSearchRipGrepArgs.getFileSearchRipGrepArgs()
+  const options = {
+    ripGrepArgs,
+    searchPath: path,
+    limit: 9999999,
+  }
+  const stdout = await SearchProcess.invoke('SearchFile.searchFile', options)
+  const lines = SplitLines.splitLines(stdout)
+  return lines
+}
