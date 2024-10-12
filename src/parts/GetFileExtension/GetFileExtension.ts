c072ac7228dae11b2c322cf74294056101072db3
Le Vivilet <levivlet@pm.me>
2024-10-12 11:19:28 +0200

    tsc

--- src/parts/GetFileExtension/GetFileExtension.ts
+++ src/parts/GetFileExtension/GetFileExtension.ts
@@ -1,16 +1,16 @@
 import * as Character from '../Character/Character.ts'
 import * as Assert from '../Assert/Assert.ts'
 
-export const getFileExtensionIndex = (file) => {
+export const getFileExtensionIndex = (file: string) => {
   Assert.string(file)
   return file.lastIndexOf(Character.Dot)
 }
 
-export const getFileExtension = (file) => {
+export const getFileExtension = (file: string) => {
   const index = getFileExtensionIndex(file)
   return file.slice(index)
 }
 
-export const getNthFileExtension = (file, startIndex) => {
+export const getNthFileExtension = (file: string, startIndex: number) => {
   return file.lastIndexOf(Character.Dot, startIndex)
 }
