342674ac42fff2027d4ce515be2858711900c983
Le Vivilet <levivlet@pm.me>
2024-10-12 11:20:47 +0200

    export

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
 import * as SearchFile from '../SearchFile/SearchFile.ts'
 import * as SearchFileWithRipGrep from '../SearchFileWithRipGrep/SearchFileWithRipGrep.ts'
+import * as SearchFileWithHtml from '../SearchFileWithHtml/SearchFileWithHtml.ts'
 
 export const commandMap = {
   'SearchFile.searchFile': SearchFile.searchFile,
   'SearchFile.searchFileWithRipGrep': SearchFileWithRipGrep.searchFile,
+  'SearchFile.searchFileWithHtml': SearchFileWithHtml.searchFileWithHtml,
 }
