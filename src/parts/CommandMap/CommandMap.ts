0d976296f6f3ae564c123c49e6eb4a237bc97135
Le Vivilet <levivlet@pm.me>
2024-10-12 09:28:53 +0200

    feature: add searchFileWithRipGrep command

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
 import * as SearchFile from '../SearchFile/SearchFile.ts'
+import * as SearchFileWithRipGrep from '../SearchFileWithRipGrep/SearchFileWithRipGrep.ts'
 
 export const commandMap = {
   'SearchFile.searchFile': SearchFile.searchFile,
+  'SearchFile.searchFileWithRipGrep': SearchFileWithRipGrep.searchFile,
 }
