caece32c9d37b322e34e16fa0d16fb50072d6a57
Le Vivilet <levivlet@pm.me>
2024-10-18 20:39:42 +0200

    feature: move more file search functionality to file search worker

--- src/parts/SearchFile/SearchFile.ts
+++ src/parts/SearchFile/SearchFile.ts
@@ -1,3 +1,23 @@
-export const searchFile = async (workspace: string, root: string): Promise<string[]> => {
-  return []
+import * as FileSystemProtocol from '../FileSystemProtocol/FileSystemProtocol.ts'
+import * as GetProtocol from '../GetProtocol/GetProtocol.ts'
+
+const getModule = (protocol: string) => {
+  switch (protocol) {
+    case FileSystemProtocol.Memfs:
+      return import('../SearchFileMemfs/SearchFileMemfs.ts')
+    case FileSystemProtocol.Fetch:
+      return import('../SearchFileWithFetch/SearchFileWithFetch.ts')
+    case FileSystemProtocol.Html:
+      return import('../SearchFileWithHtml/SearchFileWithHtml.ts')
+    default:
+      return import('../SearchFileWithRipGrep/SearchFileWithRipGrep.ts')
+  }
+}
+
+export const searchFile = async (path: string, value: string, prepare: boolean, assetDir: string): Promise<string[]> => {
+  const protocol = GetProtocol.getProtocol(path)
+  // TODO call different providers depending on protocol
+  const module = await getModule(protocol)
+  const result = await module.searchFile(path, value, prepare, assetDir)
+  return result
 }
