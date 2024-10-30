45f7f202491e28a817d0958f6fe32ac0becc8a06
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-31 00:31:26 +0100

    feature: move fileMapUrl into separate file (#32)

--- src/parts/SearchFileWithFetch/SearchFileWithFetch.ts
+++ src/parts/SearchFileWithFetch/SearchFileWithFetch.ts
@@ -1,9 +1,9 @@
+import * as FileMapUrl from '../FileMapUrl/FileMapUrl.ts'
 import * as GetJson from '../GetJson/GetJson.ts'
 import * as RemoveLeadingSlash from '../RemoveLeadingSlash/RemoveLeadingSlash.ts'
 
 export const searchFile = async (path: string, value: string, prepare: boolean, assetDir: string) => {
-  const fetchUri = `${assetDir}/config/fileMap.json`
-  const fileList = await GetJson.getJson(fetchUri)
+  const fileList = await GetJson.getJson(FileMapUrl.fileMapUrl)
   const result = fileList.map(RemoveLeadingSlash.removeLeadingSlash)
   return result
 }
