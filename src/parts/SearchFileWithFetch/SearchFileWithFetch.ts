2793ce26e3d3570b8cc8c56a9fffef72636b0aa4
Le Vivilet <levivlet@pm.me>
2024-10-31 00:22:32 +0100

    feature: move getJson into separate file

--- src/parts/SearchFileWithFetch/SearchFileWithFetch.ts
+++ src/parts/SearchFileWithFetch/SearchFileWithFetch.ts
@@ -1,3 +1,5 @@
+import * as GetJson from '../GetJson/GetJson.ts'
+
 const removeLeadingSlash = (path: string) => {
   if (path.startsWith('/')) {
     return path.slice(1)
@@ -7,11 +9,7 @@ const removeLeadingSlash = (path: string) => {
 
 export const searchFile = async (path: string, value: string, prepare: boolean, assetDir: string) => {
   const fetchUri = `${assetDir}/config/fileMap.json`
-  const response = await fetch(fetchUri)
-  if (!response.ok) {
-    throw new Error(response.statusText)
-  }
-  const fileList = await response.json()
+  const fileList = await GetJson.getJson(fetchUri)
   const result = fileList.map(removeLeadingSlash)
   return result
 }
