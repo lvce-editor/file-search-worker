79c98cf3f7aa8c85f5c40b0b1f1bfb57e99d6819
Le Vivilet <levivlet@pm.me>
2024-10-31 00:28:34 +0100

    feature: improve handling of file prefix

--- src/parts/SearchFileWithFetch/SearchFileWithFetch.ts
+++ src/parts/SearchFileWithFetch/SearchFileWithFetch.ts
@@ -1,9 +1,17 @@
 import * as GetJson from '../GetJson/GetJson.ts'
 import * as RemoveLeadingSlash from '../RemoveLeadingSlash/RemoveLeadingSlash.ts'
 
+// TODO simplify code
+// 1. don't have playground prefix in fileMap json
+// 2. remove code here that removes the prefix
 export const searchFile = async (path: string, value: string, prepare: boolean, assetDir: string) => {
   const fetchUri = `${assetDir}/config/fileMap.json`
   const fileList = await GetJson.getJson(fetchUri)
   const result = fileList.map(RemoveLeadingSlash.removeLeadingSlash)
-  return result
+  const prefixLength = path.slice('fetch:///'.length)
+  const final = []
+  for (const item of result) {
+    final.push(item.slice(prefixLength))
+  }
+  return final
 }
