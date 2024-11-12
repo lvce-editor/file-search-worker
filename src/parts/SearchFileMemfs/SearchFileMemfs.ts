0c42bd8a7ad12b7b21678a97cce58e252d659b7c
Le Vivilet <levivlet@pm.me>
2024-11-12 22:38:14 +0100

    feature: use explicit return types

--- src/parts/SearchFileMemfs/SearchFileMemfs.ts
+++ src/parts/SearchFileMemfs/SearchFileMemfs.ts
@@ -1,6 +1,6 @@
 import * as FileSystemMemory from '../FileSystemMemory/FileSystemMemory.ts'
 
-export const searchFile = async (path: string, value: string): Promise<readonly string[]> => {
+export const searchFile = async (): Promise<readonly string[]> => {
   const files = await FileSystemMemory.getFiles()
   const keys = Object.keys(files)
   return keys
--- src/parts/SearchFileWithFetch/SearchFileWithFetch.ts
+++ src/parts/SearchFileWithFetch/SearchFileWithFetch.ts
@@ -5,7 +5,7 @@ import * as RemoveLeadingSlash from '../RemoveLeadingSlash/RemoveLeadingSlash.ts
 // TODO simplify code
 // 1. don't have playground prefix in fileMap json
 // 2. remove code here that removes the prefix
-export const searchFile = async (path: string, value: string, prepare: boolean, assetDir: string): Promise<readonly string[]> => {
+export const searchFile = async (path: string): Promise<readonly string[]> => {
   const fileList = await GetJson.getJson(FileMapUrl.fileMapUrl)
   const result = fileList.map(RemoveLeadingSlash.removeLeadingSlash)
   const prefixLength = path.length - 'file:///'.length
