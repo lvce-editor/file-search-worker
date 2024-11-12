7f495e1e3f286f5d93c81b6d23b8c4c91e959e28
Le Vivilet <levivlet@pm.me>
2024-11-12 22:35:01 +0100

    config

--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/member-delimiter-style': 'off',
+      '@typescript-eslint/only-throw-error': 'off',
     },
   },
 )
