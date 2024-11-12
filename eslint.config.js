40164a2952a396c3de40b1d2bb415275d3a3b63b
Le Vivilet <levivlet@pm.me>
2024-11-12 22:27:32 +0100

    add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/member-delimiter-style': 'off',
     },
-  }
+  },
 )
