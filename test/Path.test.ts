88b90a4389b42bd7ebc4760b6514b6fda994f912
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-17 10:13:35 +0100

    feature: add path tests (#62)
    * feature: add path tests
    
    * test

---
+++ test/Path.test.ts
@@ -0,0 +1,34 @@
+import { expect, test } from '@jest/globals'
+import * as Path from '../src/parts/Path/Path.ts'
+
+test('dirname - root path', () => {
+  expect(Path.dirname('/', '/test')).toBe('')
+})
+
+test('dirname - nested path', () => {
+  expect(Path.dirname('/', '/test/file.txt')).toBe('/test')
+})
+
+test('dirname - multiple levels', () => {
+  expect(Path.dirname('/', '/test/subfolder/file.txt')).toBe('/test/subfolder')
+})
+
+test('dirname - no separator', () => {
+  expect(Path.dirname('/', 'test')).toBe('test')
+})
+
+test('dirname - empty path', () => {
+  expect(Path.dirname('/', '')).toBe('')
+})
+
+test('dirname - only separator', () => {
+  expect(Path.dirname('/', '/')).toBe('')
+})
+
+test('dirname - multiple separators', () => {
+  expect(Path.dirname('/', '/test//file.txt')).toBe('/test/')
+})
+
+test('dirname - with different separator', () => {
+  expect(Path.dirname('\\', 'C:\\test\\file.txt')).toBe('C:\\test')
+})
