10943f9fcc2c3623918955c9a000a94724cdda4e
Le Vivilet <levivlet@pm.me>
2024-11-18 21:01:27 +0100

    feature: convert application to monorepo

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -56,7 +56,7 @@ await bundleJs()
 
 const version = await getVersion()
 
-const packageJson = await readJson(join(root, 'packages', 'text-search-worker', 'package.json'))
+const packageJson = await readJson(join(root, 'packages', 'file-search-worker', 'package.json'))
 
 delete packageJson.scripts
 delete packageJson.devDependencies
@@ -66,7 +66,7 @@ delete packageJson.xo
 delete packageJson.directories
 delete packageJson.nodemonConfig
 packageJson.version = version
-packageJson.main = 'dist/textSearchWorkerMain.js'
+packageJson.main = 'dist/fileSearchWorkerMain.js'
 
 await writeJson(join(dist, 'package.json'), packageJson)
 
