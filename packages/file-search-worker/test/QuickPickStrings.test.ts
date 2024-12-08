92d0480ac4370554ed330c0b6c71a7517047895f
Le Vivilet <levivlet@pm.me>
2024-12-08 21:28:23 +0100

    feature: add quickpick strings

---
+++ packages/file-search-worker/test/QuickPickStrings.test.ts
@@ -0,0 +1,58 @@
+import { expect, test } from '@jest/globals'
+import * as QuickPickStrings from '../src/parts/QuickPickStrings/QuickPickStrings.ts'
+
+test('files returns string', () => {
+  expect(QuickPickStrings.files()).toBe('Files')
+})
+
+test('goToFile returns string', () => {
+  expect(QuickPickStrings.goToFile()).toBe('Go to file')
+})
+
+test('noMatchingColorThemesFound returns string', () => {
+  expect(QuickPickStrings.noMatchingColorThemesFound()).toBe('No matching color themes found')
+})
+
+test('noMatchingResults returns string', () => {
+  expect(QuickPickStrings.noMatchingResults()).toBe('No matching results')
+})
+
+test('noRecentlyOpenedFoldersFound returns string', () => {
+  expect(QuickPickStrings.noRecentlyOpenedFoldersFound()).toBe('No recently opened folders found')
+})
+
+test('noResults returns string', () => {
+  expect(QuickPickStrings.noResults()).toBe('No Results')
+})
+
+test('noSymbolFound returns string', () => {
+  expect(QuickPickStrings.noSymbolFound()).toBe('No symbol found')
+})
+
+test('noWorkspaceSymbolsFound returns string', () => {
+  expect(QuickPickStrings.noWorkspaceSymbolsFound()).toBe('no workspace symbols found')
+})
+
+test('openRecent returns string', () => {
+  expect(QuickPickStrings.openRecent()).toBe('Open Recent')
+})
+
+test('selectColorTheme returns string', () => {
+  expect(QuickPickStrings.selectColorTheme()).toBe('Select Color Theme')
+})
+
+test('selectToOpen returns string', () => {
+  expect(QuickPickStrings.selectToOpen()).toBe('Select to open')
+})
+
+test('showAndRunCommands returns string', () => {
+  expect(QuickPickStrings.showAndRunCommands()).toBe('Show And Run Commands')
+})
+
+test('typeNameofCommandToRun returns string', () => {
+  expect(QuickPickStrings.typeNameofCommandToRun()).toBe('Type the name of a command to run.')
+})
+
+test('typeTheNameOfAViewToOpen returns string', () => {
+  expect(QuickPickStrings.typeTheNameOfAViewToOpen()).toBe('Type the name of a view, output channel or terminal to open.')
+})
