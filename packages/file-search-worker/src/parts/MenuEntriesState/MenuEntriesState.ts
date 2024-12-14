dea3ab9f0ed23ce82bf510eb315a45904f0e798a
Le Vivilet <levivlet@pm.me>
2024-12-14 22:05:38 +0100

    remove unused code

--- packages/file-search-worker/src/parts/MenuEntriesState/MenuEntriesState.ts
+++ packages/file-search-worker/src/parts/MenuEntriesState/MenuEntriesState.ts
@@ -2,14 +2,10 @@ interface MenuEntriesState {
   menuEntries: readonly any[]
 }
 
-export const state: MenuEntriesState = {
+const state: MenuEntriesState = {
   menuEntries: [],
 }
 
 export const getAll = () => {
   return state.menuEntries
-}
-
-export const add = (menuEntries: any[]) => {
-  state.menuEntries = [...state.menuEntries, ...menuEntries]
 }
