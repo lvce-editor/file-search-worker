aee7c817342be3eb79a145459f677d1851598de8
Le Vivilet <levivlet@pm.me>
2024-12-14 22:07:51 +0100

    tsc

--- packages/file-search-worker/src/parts/GetColorThemeNames/GetColorThemeNames.ts
+++ packages/file-search-worker/src/parts/GetColorThemeNames/GetColorThemeNames.ts
@@ -1,13 +1,5 @@
-import * as AssetDir from '../AssetDir/AssetDir.ts'
-import * as Command from '../Command/Command.ts'
-import * as Platform from '../Platform/Platform.ts'
-import * as PlatformType from '../PlatformType/PlatformType.ts'
-import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
+import * as Rpc from '../Rpc/Rpc.ts'
 
 export const getColorThemeNames = async (): Promise<any[]> => {
-  if (Platform.platform === PlatformType.Web) {
-    const url = `${AssetDir.assetDir}/config/themes.json`
-    return Command.execute(/* Ajax.getJson */ 'Ajax.getJson', /* url */ url)
-  }
-  return SharedProcess.invoke(/* ExtensionHost.getColorThemeNames */ 'ExtensionHost.getColorThemeNames')
+  return Rpc.invoke(/* Ajax.getJson */ 'ColorTheme.getColorThemeNames')
 }
