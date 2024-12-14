befd788001d41d051d61efc832eb233056aad40f
Le Vivilet <levivlet@pm.me>
2024-12-14 22:17:41 +0100

    tsc

--- packages/file-search-worker/src/parts/ErrorHandling/ErrorHandling.ts
+++ packages/file-search-worker/src/parts/ErrorHandling/ErrorHandling.ts
@@ -1,9 +1,9 @@
-export const handleError = async (error: any, notify = true, prefix = '') => {
+export const handleError = async (error: any, notify = true, prefix = ''): Promise<void> => {
   console.error(error)
 }
 
-export const showErrorDialog = async () => {}
+export const showErrorDialog = async (): Promise<void> => {}
 
-export const warn = (...args: any[]) => {
+export const warn = (...args: any[]): void => {
   console.warn(...args)
 }
