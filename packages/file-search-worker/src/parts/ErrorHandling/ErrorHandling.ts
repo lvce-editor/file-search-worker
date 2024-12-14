1a74d08604c66e7997ace9d406295d8888f705e7
Le Vivilet <levivlet@pm.me>
2024-12-14 22:11:02 +0100

    clean code

--- packages/file-search-worker/src/parts/ErrorHandling/ErrorHandling.ts
+++ packages/file-search-worker/src/parts/ErrorHandling/ErrorHandling.ts
@@ -1,12 +1,8 @@
-export const logError = async (error: any, prefix = '') => {
-  console.error(error)
-}
-
 export const handleError = async (error: any, notify = true, prefix = '') => {
   console.error(error)
 }
 
-export const showErrorDialog = async (error: any) => {}
+export const showErrorDialog = async () => {}
 
 export const warn = (...args: any[]) => {
   console.warn(...args)
