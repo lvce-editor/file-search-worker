37561e7f4e765b6c70c7ea53cf6da0f6d0d29548
Le Vivilet <levivlet@pm.me>
2024-12-21 09:48:25 +0100

    feature: sort imports

--- packages/file-search-worker/src/parts/FilterQuickPickItems/FilterQuickPickItems.ts
+++ packages/file-search-worker/src/parts/FilterQuickPickItems/FilterQuickPickItems.ts
@@ -1,7 +1,7 @@
+import type { Pick } from '../Pick/Pick.ts'
 import * as ConvertToPick from '../ConvertToPick/ConvertToPick.ts'
 import * as FilterQuickPickItem from '../FilterQuickPickItem/FilterQuickPickItem.ts'
 import * as GetBaseName from '../GetBaseName/GetBaseName.ts'
-import type { Pick } from '../Pick/Pick.ts'
 
 export const filterQuickPickItems = (items: readonly string[], value: string): readonly Pick[] => {
   if (!value) {
