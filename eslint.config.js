f5ec089b64dd0830dc7431abdb5c2b2281320dac
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-15 18:11:16 +0100

    feature: use npm package for eslint config (#96)

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,9 @@
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import perfectionist from 'eslint-plugin-perfectionist'
+import config from '@lvce-editor/eslint-config'
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
+export default [
+  ...config,
   {
     ignores: [
-      'dist',
-      '.tmp',
-      'scripts/build.js',
-      'scripts/computeNodeModulesCacheKey.js',
-      '**/build/**',
-      '**/coverage/**',
-      '**/server/**',
-      '**/e2e/**',
-      '**/memory/**',
-      '**/test-integration/**',
-      '**/test-integration-util/**',
-      'rollup.config.js',
-      'eslint.config.js',
       'packages/file-search-worker/src/fileSearchWorkerMain.ts',
       'packages/file-search-worker/test/GetJson.test.ts',
       'packages/file-search-worker/test/GetJson.test.ts',
@@ -35,41 +11,9 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/consistent-type-exports': 'error',
-      '@typescript-eslint/consistent-type-imports': 'error',
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-      '@typescript-eslint/member-delimiter-style': 'off',
-      '@typescript-eslint/only-throw-error': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/require-await': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/restrict-template-expressions': 'off',
+      'prefer-destructuring': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
     },
   },
-  {
-    plugins: {
-      perfectionist,
-    },
-    rules: {
-      'perfectionist/sort-imports': [
-        'error',
-        {
-          type: 'natural',
-          order: 'asc',
-          newlinesBetween: 'never',
-        },
-      ],
-    },
-  },
-)
+]
