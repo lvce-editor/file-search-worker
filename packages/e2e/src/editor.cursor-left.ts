b257487e08182e153ec81fac8ecc06180f015dbe
Le Vivilet <levivlet@pm.me>
2025-01-24 23:39:35 +0100

    feature: use typescript for e2e tests

--- packages/e2e/src/editor.cursor-left.ts
+++ packages/e2e/src/editor.cursor-left.ts
@@ -1,6 +1,8 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
 export const name = 'viewlet.editor-cursor-word-left'
 
-export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file1.txt`, `<title>Document</title>`)
