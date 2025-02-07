1cec3c46f5c3781ec6e893eb8d583c10a59032f1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-07 22:26:03 +0100

    feature: add quickPickState interface (#105)

---
+++ packages/file-search-worker/src/parts/QuickPickState/QuickPickState.ts
@@ -0,0 +1,35 @@
+export interface QuickPickState {
+  readonly uid: number
+  readonly state: number
+  readonly picks: readonly any[]
+  readonly recentPicks: readonly any[]
+  readonly recentPickIds: Map<any, any>
+  readonly versionId: number
+  readonly provider: any
+  readonly warned: any[]
+  readonly visiblePicks: readonly any[]
+  readonly maxVisibleItems: number
+  readonly uri: string
+  readonly cursorOffset: number
+  readonly height: number
+  readonly top: number
+  readonly width: number
+  readonly deltaY: number
+  readonly minLineY: number
+  readonly maxLineY: number
+  readonly finalDeltaY: number
+  readonly headerHeight: number
+  readonly itemHeight: number
+  readonly items: readonly any[]
+  readonly minimumSliderSize: number
+  readonly focusedIndex: number
+  readonly touchOffsetY: number
+  readonly touchTimeStamp: number
+  readonly touchDifference: number
+  readonly scrollBarHeight: number
+  readonly scrollBarActive: boolean
+  readonly platform: number
+  readonly inputSource: number
+  readonly args: readonly any[]
+  readonly focused: boolean
+}
