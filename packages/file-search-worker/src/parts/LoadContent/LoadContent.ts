20dade238612b1b28fd529fdee1de5cf63e9edd6
Le Vivilet <levivlet@pm.me>
2025-02-07 22:53:58 +0100

    lint

--- packages/file-search-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/file-search-worker/src/parts/LoadContent/LoadContent.ts
@@ -2,8 +2,8 @@ import type { QuickPickState } from '../QuickPickState/QuickPickState.ts'
 import * as Assert from '../Assert/Assert.ts'
 import * as FilterQuickPickItems from '../FilterQuickPickItems/FilterQuickPickItems.ts'
 import * as GetDefaultValue from '../GetDefaultValue/GetDefaultValue.ts'
-import * as InputSource from '../InputSource/InputSource.js'
-import * as QuickPickEntries from '../QuickPickEntries/QuickPickEntries.js'
+import * as InputSource from '../InputSource/InputSource.ts'
+import * as QuickPickEntries from '../QuickPickEntries/QuickPickEntries.ts'
 import * as QuickPickOpenState from '../QuickPickOpenState/QuickPickOpenState.ts'
 
 export const loadContent = async (state: QuickPickState): Promise<QuickPickState> => {
