746e3f709d03089fbfa02711e9d04d281c8045d8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-19 22:49:26 +0100

    feature: use provider.getLabel if available (#179)
    * feature: use provider.getLabel if available
    
    * lint

--- packages/file-search-worker/src/parts/FilterQuickPickItems/FilterQuickPickItems.ts
+++ packages/file-search-worker/src/parts/FilterQuickPickItems/FilterQuickPickItems.ts
@@ -1,16 +1,21 @@
 import type { Pick } from '../Pick/Pick.ts'
 import * as ConvertToPick from '../ConvertToPick/ConvertToPick.ts'
 import * as FilterQuickPickItem from '../FilterQuickPickItem/FilterQuickPickItem.ts'
-import * as GetBaseName from '../GetBaseName/GetBaseName.ts'
+import { pathBaseName } from '../Workspace/Workspace.ts'
 
-export const filterQuickPickItems = (items: readonly string[], value: string): readonly Pick[] => {
+export const filterQuickPickItems = (items: readonly string[], value: string, provider?: any): readonly Pick[] => {
   if (!value) {
     return items.map(ConvertToPick.convertToPick)
   }
   const results: Pick[] = []
   for (const item of items) {
-    const baseName = GetBaseName.getBaseName(item)
-    const matches = FilterQuickPickItem.filterQuickPickItem(value, baseName)
+    let filterValue = ''
+    if (provider) {
+      filterValue = provider.getPickLabel(item)
+    } else {
+      filterValue = pathBaseName(item)
+    }
+    const matches = FilterQuickPickItem.filterQuickPickItem(value, filterValue)
     if (matches.length > 0) {
       results.push({
         pick: item,
