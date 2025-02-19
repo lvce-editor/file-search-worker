e5c82f5f4385be415e7b1d84b3b2dd116e8d52f1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-19 22:00:25 +0100

    feature: add function to create viewmodel (#171)

---
+++ packages/file-search-worker/src/parts/CreateQuickPickViewModel/CreateQuickPickViewModel.ts
@@ -0,0 +1,27 @@
+import type { QuickPickState } from '../QuickPickState/QuickPickState.ts'
+import type { QuickPickViewModel } from '../QuickPickViewModel/QuickPickViewModel.ts'
+import * as GetVisibleQuickPickItems from '../GetVisibleQuickPickItems/GetVisibleQuickPickItems.ts'
+
+export const createQuickPickViewModel = (oldState: QuickPickState, newState: QuickPickState): QuickPickViewModel => {
+  const visibleItems = GetVisibleQuickPickItems.getVisible(
+    newState.provider,
+    newState.items,
+    newState.minLineY,
+    newState.maxLineY,
+    newState.focusedIndex,
+  )
+  const oldFocusedIndex = oldState.focusedIndex - oldState.minLineY
+  const newFocusedIndex = newState.focusedIndex - newState.minLineY
+  const maxLineY = Math.min(newState.maxLineY, newState.items.length)
+  const itemCount = maxLineY - newState.minLineY
+  const height = itemCount * newState.itemHeight
+  return {
+    visibleItems,
+    value: newState.value,
+    cursorOffset: newState.cursorOffset,
+    focused: newState.focused,
+    height,
+    oldFocusedIndex,
+    newFocusedIndex,
+  }
+}
