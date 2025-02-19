872f089ce3b5653709924f4809dd3a97faa2c81b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-19 19:41:08 +0100

    feature: simplify memory measurement (#131)
    * feature: simplify memory measurement
    
    * simplify code

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -1,6 +1,8 @@
 import { join } from 'node:path'
 import { root } from './root.ts'
 
-export const threshold = 500_000
+export const threshold = 470_000
 
 export const workerPath = join(root, '.tmp/dist/dist/fileSearchWorkerMain.js')
+
+export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
