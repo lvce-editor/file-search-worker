b9b5cd193977210238c4c6f91381809ad7cedc51
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-19 20:39:27 +0100

    feature: register blur command (#153)

--- packages/file-search-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/file-search-worker/src/parts/CommandMap/CommandMap.ts
@@ -6,6 +6,7 @@ import * as FilterQuickPickItems from '../FilterQuickPickItems/FilterQuickPickIt
 import * as FocusIndex from '../FocusIndex/FocusIndex.ts'
 import * as FocusNext from '../FocusNext/FocusNext.ts'
 import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
+import * as HandleBlur from '../HandleBlur/HandleBlur.ts'
 import * as HandleWheel from '../HandleWheel/HandleWheel.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as LoadQuickPickEntries from '../LoadQuickPickEntries/LoadQuickPickEntries.ts'
@@ -23,6 +24,7 @@ export const commandMap = {
   'QuickPick.focusIndex': WrapCommand.wrapCommand(FocusIndex.focusIndex),
   'QuickPick.focusNext': WrapCommand.wrapCommand(FocusNext.focusNext),
   'QuickPick.getKeyBindings': GetKeyBindings.getKeyBindings,
+  'QuickPick.handleBlur': WrapCommand.wrapCommand(HandleBlur.handleBlur),
   'QuickPick.handleWheel': WrapCommand.wrapCommand(HandleWheel.handleWheel),
   'QuickPick.loadContent': WrapCommand.wrapCommand(LoadContent.loadContent),
   'QuickPick.loadEntries2': QuickPickEntries.load,
