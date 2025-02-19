fc37e7b8ffa2d0a37a1b137e9cec55bb35d17eb9
Le Vivilet <levivlet@pm.me>
2025-02-19 22:12:10 +0100

    render

--- packages/file-search-worker/src/parts/Render/Render.ts
+++ packages/file-search-worker/src/parts/Render/Render.ts
@@ -2,7 +2,7 @@ import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as Diff from '../Diff/Diff.ts'
 import * as QuickPickStates from '../QuickPickStates/QuickPickStates.ts'
 
-export const doRender = (uid: number): readonly any[] => {
+export const doRender = async (uid: number): Promise<readonly any[]> => {
   const { oldState, newState } = QuickPickStates.get(uid)
   const diffResult = Diff.diff(oldState, newState)
   return ApplyRender.applyRender(oldState, newState, diffResult)
