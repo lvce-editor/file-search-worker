3343ae92c1283c97e975c6f8dc97e8bb85faee4c
Le Vivilet <levivlet@pm.me>
2025-02-19 20:20:45 +0100

    feature: add handleInput command

---
+++ packages/file-search-worker/src/parts/HandleInput/HandleInput.ts
@@ -0,0 +1,30 @@
+import type { QuickPickState } from '../QuickPickState/QuickPickState.ts'
+import * as FilterQuickPickItems from '../FilterQuickPickItems/FilterQuickPickItems.ts'
+import * as InputSource from '../InputSource/InputSource.js'
+
+// TODO when user types letters -> no need to query provider again -> just filter existing results
+export const handleInput = async (
+  state: QuickPickState,
+  newValue: string,
+  cursorOffset: number,
+  inputSource = InputSource.Script,
+): Promise<QuickPickState> => {
+  if (state.value === newValue) {
+    return state
+  }
+  // @ts-ignore
+  state.value = newValue
+  // @ts-ignore
+  state.inputSource = inputSource
+  const newPicks = await state.provider.getPicks(newValue)
+  const filterValue = state.provider.getFilterValue(newValue)
+  const items = FilterQuickPickItems.filterQuickPickItems(newPicks, filterValue)
+  const focusedIndex = items.length === 0 ? -1 : 0
+  return {
+    ...state,
+    picks: newPicks,
+    items,
+    focusedIndex,
+    cursorOffset,
+  }
+}
