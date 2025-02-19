34581a8772bcd0f5409f0d4320b95b3094de6d2f
Le Vivilet <levivlet@pm.me>
2025-02-19 20:25:28 +0100

    feature: add selectIndex function

---
+++ packages/file-search-worker/src/parts/SelectIndex/SelectIndex.ts
@@ -0,0 +1,32 @@
+import * as Assert from '../Assert/Assert.ts'
+import * as CloseWidget from '../CloseWidget/CloseWidget.ts'
+import * as GetPick from '../GetPick/GetPick.ts'
+import * as QuickPickReturnValue from '../QuickPickReturnValue/QuickPickReturnValue.ts'
+import { QuickPickState } from '../QuickPickState/QuickPickState.ts'
+
+export const selectIndex = async (state: QuickPickState, index: number, button = /* left */ 0): Promise<QuickPickState> => {
+  const { minLineY, provider, items } = state
+  const actualIndex = index + minLineY
+  const pick = GetPick.getPick(items, actualIndex)
+  const selectPickResult = await provider.selectPick(pick, actualIndex, button)
+  Assert.object(selectPickResult)
+  Assert.string(selectPickResult.command)
+  const { command } = selectPickResult
+  switch (command) {
+    case QuickPickReturnValue.Hide:
+      await CloseWidget.closeWidget(state.uid)
+      return state
+    default:
+      return state
+  }
+
+  // TODO recent picks should be per provider
+  // if (!state.recentPickIds.has(pick.id)) {
+  //   state.recentPicks.unshift(pick)
+  //   state.recentPickIds.add(pick.id)
+  // }
+  // if (state.recentPicks.length > RECENT_PICKS_MAX_SIZE) {
+  //   const last = state.recentPicks.pop()
+  //   state.recentPickIds.delete(last.id)
+  // }
+}
