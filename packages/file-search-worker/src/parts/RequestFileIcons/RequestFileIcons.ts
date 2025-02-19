940b031d0142b18c96736395b7ad6558618d6218
Le Vivilet <levivlet@pm.me>
2025-02-19 22:38:21 +0100

    feature: don't try to request file icon for empty string

--- packages/file-search-worker/src/parts/RequestFileIcons/RequestFileIcons.ts
+++ packages/file-search-worker/src/parts/RequestFileIcons/RequestFileIcons.ts
@@ -3,10 +3,13 @@ import * as DirentType from '../DirentType/DirentType.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
 
 export const requestFileIcons = async (requests: readonly IconRequest[]): Promise<readonly string[]> => {
-  const promises = requests.map((request) =>
-    request.type === DirentType.File
+  const promises = requests.map((request) => {
+    if (!request.name) {
+      return ''
+    }
+    return request.type === DirentType.File
       ? Rpc.invoke('IconTheme.getFileIcon', { name: request.name })
-      : Rpc.invoke('IconTheme.getFolderIcon', { name: request.name }),
-  )
+      : Rpc.invoke('IconTheme.getFolderIcon', { name: request.name })
+  })
   return Promise.all(promises)
 }
