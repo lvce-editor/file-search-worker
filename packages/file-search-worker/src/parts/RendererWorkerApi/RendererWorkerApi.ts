91c4997e9d21ffa567b1f9005250f5338cb243dc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-21 19:41:05 +0100

    feature: improve renderer worker api (#208)

--- packages/file-search-worker/src/parts/RendererWorkerApi/RendererWorkerApi.ts
+++ packages/file-search-worker/src/parts/RendererWorkerApi/RendererWorkerApi.ts
@@ -1,17 +1,18 @@
 export interface RendererWorkerApi {
   readonly 'ClipBoard.writeText': (text: string) => Promise<void>
+  readonly 'ColorTheme.getColorThemeNames': () => Promise<readonly string[]>
+  readonly 'ColorTheme.setColorTheme': (id: string) => Promise<void>
   readonly 'ContextMenu.show': (x: number, y: number, id: any, ...args: readonly any[]) => Promise<void>
+  readonly 'ExtensionHost.executeCommand': (id: string) => Promise<void>
+  readonly 'ExtensionHost.getCommands': () => Promise<readonly any[]>
   readonly 'Focus.setFocus': (focusKey: number) => Promise<void>
   readonly 'IconTheme.getFileIcon': (options: any) => Promise<string>
   readonly 'IconTheme.getFolderIcon': (options: any) => Promise<string>
   readonly 'Main.openUri': (uri: string, focus?: boolean, props?: any) => Promise<void>
-  readonly 'SearchProcess.invoke': (command: string, options: any, ...args: readonly any[]) => Promise<any>
-  readonly 'Workspace.setPath': (uri: string) => Promise<void>
-  readonly 'Workspace.getPath': () => Promise<string>
   readonly 'RecentlyOpened.getRecentlyOpened': () => Promise<readonly string[]>
-  readonly 'ExtensionHost.executeCommand': (id: string) => Promise<void>
-  readonly 'Viewlet.closeWidget': (id: string) => Promise<void>
-  readonly 'ColorTheme.setColorTheme': (id: string) => Promise<void>
-  readonly 'ExtensionHost.getCommands': () => Promise<readonly any[]>
-  readonly 'ColorTheme.getColorThemeNames': () => Promise<readonly string[]>
+  readonly 'SearchProcess.invoke': (command: string, ...args: readonly any[]) => Promise<any>
+  readonly 'Viewlet.closeWidget': (id: number) => Promise<void>
+  readonly 'Viewlet.openWidget': (id: string) => Promise<void>
+  readonly 'Workspace.getPath': () => Promise<string>
+  readonly 'Workspace.setPath': (uri: string) => Promise<void>
 }
