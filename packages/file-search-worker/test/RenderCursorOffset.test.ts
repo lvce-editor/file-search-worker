3b35605661f0985ca1bcba4c9a6d73b13dca36b1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-21 19:23:23 +0100

    feature: add renderCursor test (#201)

---
+++ packages/file-search-worker/test/RenderCursorOffset.test.ts
@@ -0,0 +1,46 @@
+import { expect, test } from '@jest/globals'
+import type { QuickPickViewModel } from '../src/parts/QuickPickViewModel/QuickPickViewModel.ts'
+import * as RenderCursorOffset from '../src/parts/RenderCursorOffset/RenderCursorOffset.ts'
+import * as RenderMethod from '../src/parts/RenderMethod/RenderMethod.ts'
+
+test('renders cursor offset', () => {
+  const state: QuickPickViewModel = {
+    uid: 1,
+    cursorOffset: 5,
+    visibleItems: [],
+    value: '',
+    oldFocusedIndex: 0,
+    newFocusedIndex: 0,
+    height: 0,
+    focused: false,
+  }
+  expect(RenderCursorOffset.renderCursorOffset(state)).toEqual(['Viewlet.send', 1, RenderMethod.SetCursorOffset, 5])
+})
+
+test('renders cursor offset of 0', () => {
+  const state: QuickPickViewModel = {
+    uid: 1,
+    cursorOffset: 0,
+    visibleItems: [],
+    value: '',
+    oldFocusedIndex: 0,
+    newFocusedIndex: 0,
+    height: 0,
+    focused: false,
+  }
+  expect(RenderCursorOffset.renderCursorOffset(state)).toEqual(['Viewlet.send', 1, RenderMethod.SetCursorOffset, 0])
+})
+
+test('handles different uid', () => {
+  const state: QuickPickViewModel = {
+    uid: 2,
+    cursorOffset: 10,
+    visibleItems: [],
+    value: '',
+    oldFocusedIndex: 0,
+    newFocusedIndex: 0,
+    height: 0,
+    focused: false,
+  }
+  expect(RenderCursorOffset.renderCursorOffset(state)).toEqual(['Viewlet.send', 2, RenderMethod.SetCursorOffset, 10])
+})
