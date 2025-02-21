56cb6e8459ef1d870fcb57229dcebb556c4b5e01
Le Vivilet <levivlet@pm.me>
2025-02-21 19:24:29 +0100

    lint

--- packages/file-search-worker/test/GetRenderer.test.ts
+++ packages/file-search-worker/test/GetRenderer.test.ts
@@ -1,12 +1,12 @@
 import { expect, test } from '@jest/globals'
-import * as GetRenderer from '../src/parts/GetRenderer/GetRenderer.ts'
 import * as DiffType from '../src/parts/DiffType/DiffType.ts'
-import * as RenderValue from '../src/parts/RenderValue/RenderValue.ts'
+import * as GetRenderer from '../src/parts/GetRenderer/GetRenderer.ts'
 import * as RenderCursorOffset from '../src/parts/RenderCursorOffset/RenderCursorOffset.ts'
-import * as RenderItems from '../src/parts/RenderItems/RenderItems.ts'
+import * as RenderFocus from '../src/parts/RenderFocus/RenderFocus.ts'
 import * as RenderFocusedIndex from '../src/parts/RenderFocusedIndex/RenderFocusedIndex.ts'
 import * as RenderHeight from '../src/parts/RenderHeight/RenderHeight.ts'
-import * as RenderFocus from '../src/parts/RenderFocus/RenderFocus.ts'
+import * as RenderItems from '../src/parts/RenderItems/RenderItems.ts'
+import * as RenderValue from '../src/parts/RenderValue/RenderValue.ts'
 
 test('returns correct renderer for RenderValue', () => {
   const renderer = GetRenderer.getRenderer(DiffType.RenderValue)
