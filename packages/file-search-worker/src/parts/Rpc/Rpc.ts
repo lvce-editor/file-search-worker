6b962712b14cb83eff89de6f8b7d2239ed5fb094
Le Vivilet <levivlet@pm.me>
2025-02-21 19:42:50 +0100

    feature: use new rpc registry

--- packages/file-search-worker/src/parts/Rpc/Rpc.ts
+++ packages/file-search-worker/src/parts/Rpc/Rpc.ts
@@ -1,14 +1,21 @@
-const state = {
-  rpc: undefined,
-}
+import type { RendererWorkerApi } from '../RendererWorkerApi/RendererWorkerApi.ts'
+import * as RpcId from '../RpcId/RpcId.ts'
+import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
-// TODO use rpc registry
-export const invoke = (method: string, ...params: readonly any[]): Promise<any> => {
-  const rpc = state.rpc
+export const invoke = <T extends keyof RendererWorkerApi>(
+  method: T,
+  ...params: Parameters<RendererWorkerApi[T]>
+): ReturnType<RendererWorkerApi[T]> => {
+  const rpc = RpcRegistry.get(RpcId.RendererWorker)
   // @ts-ignore
   return rpc.invoke(method, ...params)
 }
 
-export const setRpc = (rpc: any): void => {
-  state.rpc = rpc
+export const invokeAndTransfer = <T extends keyof RendererWorkerApi>(
+  method: T,
+  ...params: Parameters<RendererWorkerApi[T]>
+): ReturnType<RendererWorkerApi[T]> => {
+  const rpc = RpcRegistry.get(RpcId.RendererWorker)
+  // @ts-ignore
+  return rpc.invokeAndTransfer(method, ...params)
 }
---
+++ packages/file-search-worker/test/RequestFileIcons.test.ts
@@ -0,0 +1,62 @@
+import { expect, test, jest, beforeEach } from '@jest/globals'
+import * as DirentType from '../src/parts/DirentType/DirentType.ts'
+
+const mockInvoke = jest.fn()
+
+jest.unstable_mockModule('../src/parts/Rpc/Rpc.ts', () => ({
+  invoke: mockInvoke,
+}))
+
+const RequestFileIcons = await import('../src/parts/RequestFileIcons/RequestFileIcons.ts')
+
+beforeEach(() => {
+  mockInvoke.mockReset()
+})
+
+test('requests file icons', async () => {
+  // @ts-ignore
+  mockInvoke.mockResolvedValueOnce('/icons/file.png').mockResolvedValueOnce('/icons/other.png')
+
+  const requests = [
+    { type: DirentType.File, name: 'test.txt', path: '' },
+    { type: DirentType.File, name: 'other.txt', path: '' },
+  ]
+
+  const result = await RequestFileIcons.requestFileIcons(requests)
+  expect(result).toEqual(['/icons/file.png', '/icons/other.png'])
+  expect(mockInvoke).toHaveBeenCalledTimes(2)
+  expect(mockInvoke).toHaveBeenCalledWith('IconTheme.getFileIcon', { name: 'test.txt' })
+  expect(mockInvoke).toHaveBeenCalledWith('IconTheme.getFileIcon', { name: 'other.txt' })
+})
+
+test('requests folder icons', async () => {
+  // @ts-ignore
+  mockInvoke.mockResolvedValueOnce('/icons/folder.png').mockResolvedValueOnce('/icons/folder2.png')
+
+  const requests = [
+    { type: DirentType.Directory, name: 'folder1', path: '' },
+    { type: DirentType.Directory, name: 'folder2', path: '' },
+  ]
+
+  const result = await RequestFileIcons.requestFileIcons(requests)
+  expect(result).toEqual(['/icons/folder.png', '/icons/folder2.png'])
+  expect(mockInvoke).toHaveBeenCalledWith('IconTheme.getFolderIcon', { name: 'folder1' })
+  expect(mockInvoke).toHaveBeenCalledWith('IconTheme.getFolderIcon', { name: 'folder2' })
+})
+
+test.skip('handles empty requests array', async () => {
+  const result = await RequestFileIcons.requestFileIcons([])
+  expect(result).toEqual([])
+  expect(mockInvoke).not.toHaveBeenCalled()
+})
+
+test.skip('handles requests with no name', async () => {
+  const requests = [
+    { type: DirentType.File, name: '', path: '' },
+    { type: DirentType.Directory, name: '', path: '' },
+  ]
+
+  const result = await RequestFileIcons.requestFileIcons(requests)
+  expect(result).toEqual(['', ''])
+  expect(mockInvoke).not.toHaveBeenCalled()
+})
