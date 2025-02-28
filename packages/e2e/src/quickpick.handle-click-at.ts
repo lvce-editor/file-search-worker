4deb04056aaaf46f74af41230c461651b94e0f46
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-28 21:11:05 +0100

    feature: add click at e2e test (#226)

--- packages/e2e/src/quickpick.handle-click-at.ts
+++ packages/e2e/src/quickpick.handle-click-at.ts
@@ -2,10 +2,17 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'quickpick.handle-click-at'
 
-export const test: Test = async ({ Locator, expect, QuickPick }) => {
+export const test: Test = async ({ Locator, expect, QuickPick, Command }) => {
   // arrange
   await QuickPick.open()
-  await QuickPick.setValue('About')
+  await QuickPick.setValue('> About')
+
+  // act
+  await Command.execute('QuickPick.handleClickAt', 0, 80)
 
   // assert
+  const dialogContent = Locator('.DialogContent')
+  await expect(dialogContent).toBeVisible()
+  const infoIcon = dialogContent.locator('.DialogInfoIcon')
+  await expect(infoIcon).toBeVisible()
 }
