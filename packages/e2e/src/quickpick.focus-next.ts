8b8d04ca6302e0ba0d48ee4a5735e04f3f7dec7c
Le Vivilet <levivlet@pm.me>
2025-03-01 10:54:49 +0100

    feature: add focusNext test

---
+++ packages/e2e/src/quickpick.focus-next.ts
@@ -0,0 +1,20 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'quickpick.focus-next'
+
+export const skip = 1
+
+export const test: Test = async ({ Locator, expect, QuickPick, Command }) => {
+  // arrange
+  await QuickPick.open()
+  await QuickPick.setValue('Layout')
+
+  // act
+  // await Command.execute('QuickPick.selectIndex', 0)
+
+  // assert
+  // const dialogContent = Locator('.DialogContent')
+  // await expect(dialogContent).toBeVisible()
+  // const infoIcon = dialogContent.locator('.DialogInfoIcon')
+  // await expect(infoIcon).toBeVisible()
+}
