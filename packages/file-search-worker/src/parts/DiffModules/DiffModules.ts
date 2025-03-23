a69222304a9bffc52946d9171cfdc159c8586d5e
Le Vivilet <levivlet@pm.me>
2025-03-23 10:15:43 +0100

    feature: add diff numbers

--- packages/file-search-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/file-search-worker/src/parts/DiffModules/DiffModules.ts
@@ -4,4 +4,6 @@ import * as DiffHeight from '../DiffHeight/DiffHeight.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
-export const modules = [DiffHeight, DiffItems, DiffFocus, DiffFocus, DiffValue, DiffFocusedIndex]
+export const modules = [DiffHeight, DiffItems, DiffFocus, DiffValue, DiffFocusedIndex]
+
+export const numbers = [DiffHeight.diffType, DiffItems.diffType, DiffFocus.diffType, DiffValue.diffType, DiffFocusedIndex.diffType]
