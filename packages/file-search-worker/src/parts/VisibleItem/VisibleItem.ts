90d33185749efbb7ececdc87bb6f0a2a46f9f56d
Le Vivilet <levivlet@pm.me>
2025-03-29 20:37:48 +0100

    feature: add visibleItem interface

---
+++ packages/file-search-worker/src/parts/VisibleItem/VisibleItem.ts
@@ -0,0 +1,10 @@
+export interface VisibleItem {
+  readonly label: string
+  readonly description: string
+  readonly icon: string
+  readonly fileIcon: string
+  readonly posInSet: number
+  readonly setSize: number
+  readonly isActive: boolean
+  readonly matches: readonly string[]
+}
