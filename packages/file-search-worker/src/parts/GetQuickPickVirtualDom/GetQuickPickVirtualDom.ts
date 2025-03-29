641fdaa996fd0cee2335c22b2e6e7dca4e35287c
Le Vivilet <levivlet@pm.me>
2025-03-29 12:14:01 +0100

    feature: add items wrapper dom node

--- packages/file-search-worker/src/parts/GetQuickPickVirtualDom/GetQuickPickVirtualDom.ts
+++ packages/file-search-worker/src/parts/GetQuickPickVirtualDom/GetQuickPickVirtualDom.ts
@@ -1,20 +1,29 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetQuickPickHeaderVirtualDom from '../GetQuickPickHeaderVirtualDom/GetQuickPickHeaderVirtualDom.ts'
 import * as GetQuickPickItemsVirtualDom from '../GetQuickPickItemsVirtualDom/GetQuickPickItemsVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as QuickPickStrings from '../QuickPickStrings/QuickPickStrings.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getQuickPickVirtualDom = (visibleItems: readonly any[]): readonly VirtualDomNode[] => {
+  const quickOpen = QuickPickStrings.quickOpen()
   return [
     {
       type: VirtualDomElements.Div,
       className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.QuickPick),
       childCount: 2,
       id: 'QuickPick',
-      ariaLabel: 'Quick open',
+      ariaLabel: quickOpen,
     },
     ...GetQuickPickHeaderVirtualDom.getQuickPickHeaderVirtualDom(),
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.QuickPickItems,
+      id: 'QuickPickItems',
+      role: AriaRoles.ListBox,
+    },
     ...GetQuickPickItemsVirtualDom.getQuickPickItemsVirtualDom(visibleItems),
   ]
 }
