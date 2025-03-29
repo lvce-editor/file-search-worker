acc7494be2df9f3a2d38a7d09067f59e1795f446
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-29 12:20:05 +0100

    feature: add name to input field (#282)
    * feature: add name to input field
    
    * lint

--- packages/file-search-worker/src/parts/GetQuickPickHeaderVirtualDom/GetQuickPickHeaderVirtualDom.ts
+++ packages/file-search-worker/src/parts/GetQuickPickHeaderVirtualDom/GetQuickPickHeaderVirtualDom.ts
@@ -2,6 +2,7 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as InputName from '../InputName/InputName.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getQuickPickHeaderVirtualDom = (): readonly VirtualDomNode[] => {
@@ -19,6 +20,7 @@ export const getQuickPickHeaderVirtualDom = (): readonly VirtualDomNode[] => {
       inputType: 'text',
       childCount: 0,
       role: AriaRoles.ComboBox,
+      name: InputName.QuickPickInput,
       onBeforeInput: DomEventListenerFunctions.HandleBeforeInput,
     },
   ]
