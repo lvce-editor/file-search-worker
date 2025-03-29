332a7c8aab6321ca1071f51c026f8f37d7b1c4d0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-29 18:03:50 +0100

    feature: move input dom into separate file (#304)

--- packages/file-search-worker/src/parts/GetQuickPickHeaderVirtualDom/GetQuickPickHeaderVirtualDom.ts
+++ packages/file-search-worker/src/parts/GetQuickPickHeaderVirtualDom/GetQuickPickHeaderVirtualDom.ts
@@ -1,34 +1,15 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import * as InputName from '../InputName/InputName.ts'
-import * as QuickPickStrings from '../QuickPickStrings/QuickPickStrings.ts'
+import * as GetQuickPickInputVirtualDom from '../GetQuickPickInputVirtualDom/GetQuickPickInputVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getQuickPickHeaderVirtualDom = (): readonly VirtualDomNode[] => {
-  const ariaLabel = QuickPickStrings.typeNameofCommandToRun()
   return [
     {
       type: VirtualDomElements.Div,
       className: ClassNames.QuickPickHeader,
       childCount: 1,
     },
-    {
-      type: VirtualDomElements.Input,
-      className: ClassNames.InputBox,
-      spellcheck: false,
-      autocapitalize: 'off',
-      inputType: 'text',
-      childCount: 0,
-      role: AriaRoles.ComboBox,
-      name: InputName.QuickPickInput,
-      onBeforeInput: DomEventListenerFunctions.HandleBeforeInput,
-      onBlur: DomEventListenerFunctions.HandleBlur,
-      onInput: DomEventListenerFunctions.HandleInput,
-      ariaLabel: ariaLabel,
-      ariaAutoComplete: 'list',
-      ariaExpanded: true,
-    },
+    GetQuickPickInputVirtualDom.getQuickPickInputVirtualDom(),
   ]
 }
