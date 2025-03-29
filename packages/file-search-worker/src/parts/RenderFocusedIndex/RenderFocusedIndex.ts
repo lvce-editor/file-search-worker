532582e686b906f82b94fc5276ced3a9ed21fe90
Le Vivilet <levivlet@pm.me>
2025-03-29 08:56:56 +0100

    feature: use more exact types

--- packages/file-search-worker/src/parts/RenderFocusedIndex/RenderFocusedIndex.ts
+++ packages/file-search-worker/src/parts/RenderFocusedIndex/RenderFocusedIndex.ts
@@ -1,7 +1,7 @@
 import type { QuickPickViewModel } from '../QuickPickViewModel/QuickPickViewModel.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
 
-export const renderFocusedIndex = (newState: QuickPickViewModel): any => {
+export const renderFocusedIndex = (newState: QuickPickViewModel): readonly any[] => {
   return [
     'Viewlet.send',
     newState.uid,
