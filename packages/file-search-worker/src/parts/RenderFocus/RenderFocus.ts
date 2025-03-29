58a60ab556ef7eaf5711f6ecb898436b105402fe
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-29 12:47:47 +0100

    feature: render focus differently when renderAllItems options is enabled (#293)

--- packages/file-search-worker/src/parts/RenderFocus/RenderFocus.ts
+++ packages/file-search-worker/src/parts/RenderFocus/RenderFocus.ts
@@ -1,6 +1,10 @@
 import type { QuickPickViewModel } from '../QuickPickViewModel/QuickPickViewModel.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 export const renderFocus = (newState: QuickPickViewModel): readonly any[] => {
+  if (newState.renderAllItems) {
+    return ['Viewlet.focusElementByName', InputName.QuickPickInput]
+  }
   const selector = newState.focused ? '.InputBox' : ''
   return ['Viewlet.focusSelector', selector]
 }
