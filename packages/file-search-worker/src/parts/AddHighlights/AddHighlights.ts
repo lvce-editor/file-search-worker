d23e430d358a6640f1806748fb44df695b186526
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-29 20:42:11 +0100

    feature: move addhighlights into separate file (#315)

---
+++ packages/file-search-worker/src/parts/AddHighlights/AddHighlights.ts
@@ -0,0 +1,38 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+
+const quickPickHighlight = {
+  type: VirtualDomElements.Span,
+  className: ClassNames.QuickPickHighlight,
+  childCount: 1,
+}
+
+export const addHighlights = (dom: VirtualDomNode[], highlights: readonly any[], label: string): void => {
+  const labelDom = {
+    type: VirtualDomElements.Div,
+    className: ClassNames.QuickPickItemLabel,
+    childCount: 0,
+  }
+  dom.push(labelDom)
+  let position = 0
+  for (let i = 0; i < highlights.length; i += 2) {
+    const highlightStart = highlights[i]
+    const highlightEnd = highlights[i + 1]
+    if (position < highlightStart) {
+      const beforeText = label.slice(position, highlightStart)
+      labelDom.childCount++
+      dom.push(text(beforeText))
+    }
+    const highlightText = label.slice(highlightStart, highlightEnd)
+    labelDom.childCount++
+    dom.push(quickPickHighlight, text(highlightText))
+    position = highlightEnd
+  }
+  if (position < label.length) {
+    const afterText = label.slice(position)
+    labelDom.childCount++
+    dom.push(text(afterText))
+  }
+}
