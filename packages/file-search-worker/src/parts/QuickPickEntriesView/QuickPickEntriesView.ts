7be6d47b8ebbacedaf7d522b89df7f3d3f71b8ad
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-29 10:30:29 +0100

    feature: add view types (#261)

--- packages/file-search-worker/src/parts/QuickPickEntriesView/QuickPickEntriesView.ts
+++ packages/file-search-worker/src/parts/QuickPickEntriesView/QuickPickEntriesView.ts
@@ -27,3 +27,31 @@ export const selectPick = async (item: any): Promise<any> => {
 export const getFilterValue = (value: any): string => {
   return value
 }
+
+export const getPickFilterValue = (value: string): string => {
+  return value
+}
+
+export const getPickLabel = (value: string): string => {
+  return value
+}
+
+export const getPickIcon = (value: string): string => {
+  return ''
+}
+
+export const state = {}
+
+export const getNoResults = (): string => {
+  return ''
+}
+
+export const name = ''
+
+export const isPrepared = (): boolean => {
+  return true
+}
+
+export const getPickDescription = (): string => {
+  return ''
+}
