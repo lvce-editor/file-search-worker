89e4c0ab91cef0b1d9639e5e54c54a8c54730661
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-29 09:01:43 +0100

    feature: use more exact types (#253)

--- packages/file-search-worker/src/parts/RenderValue/RenderValue.ts
+++ packages/file-search-worker/src/parts/RenderValue/RenderValue.ts
@@ -1,6 +1,6 @@
 import type { QuickPickViewModel } from '../QuickPickViewModel/QuickPickViewModel.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
 
-export const renderValue = (newState: QuickPickViewModel): any => {
+export const renderValue = (newState: QuickPickViewModel): readonly any[] => {
   return ['Viewlet.send', newState.uid, /* method */ RenderMethod.SetValue, /* value */ newState.value]
 }
