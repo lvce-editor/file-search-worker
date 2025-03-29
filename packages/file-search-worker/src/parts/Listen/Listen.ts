867c260d5cf78d75359a1eccd54b49af3befbb83
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-29 10:36:31 +0100

    feature: register quickpick modules differently to only depend on interfaces but not the implementation (#266)

--- packages/file-search-worker/src/parts/Listen/Listen.ts
+++ packages/file-search-worker/src/parts/Listen/Listen.ts
@@ -1,5 +1,7 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import * as QuickPickEntries from '../QuickPickEntries/QuickPickEntries.ts'
+import * as QuickPickEntriesModules from '../QuickPickEntriesModules/QuickPickEntriesModules.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 import * as SearchFileModule from '../SearchFileModule/SearchFileModule.ts'
@@ -7,6 +9,7 @@ import * as SearchModules from '../SearchModules/SearchModules.ts'
 
 export const listen = async (): Promise<void> => {
   SearchFileModule.register(SearchModules.searchModules)
+  QuickPickEntries.register(QuickPickEntriesModules.quickPickEntriesModules)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
