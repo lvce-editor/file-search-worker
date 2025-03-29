eb05b930e1485ac1609867410452b49da47fe19d
Le Vivilet <levivlet@pm.me>
2025-03-29 12:17:42 +0100

    lint

--- packages/file-search-worker/src/parts/GetQuickPickHeaderVirtualDom/GetQuickPickHeaderVirtualDom.ts
+++ packages/file-search-worker/src/parts/GetQuickPickHeaderVirtualDom/GetQuickPickHeaderVirtualDom.ts
@@ -1,8 +1,8 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as InputName from '../InputName/InputName.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as InputName from '../InputName/InputName.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getQuickPickHeaderVirtualDom = (): readonly VirtualDomNode[] => {
