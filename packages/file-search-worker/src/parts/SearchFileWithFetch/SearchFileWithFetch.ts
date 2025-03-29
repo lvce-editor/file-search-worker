4729425dacc81939c9d49249f99f74d5b06fd761
Le Vivilet <levivlet@pm.me>
2025-03-29 11:15:32 +0100

    feature: ask extension host for fetch search results

--- packages/file-search-worker/src/parts/SearchFileWithFetch/SearchFileWithFetch.ts
+++ packages/file-search-worker/src/parts/SearchFileWithFetch/SearchFileWithFetch.ts
@@ -1,17 +1,8 @@
-import * as FileMapUrl from '../FileMapUrl/FileMapUrl.ts'
-import * as GetJson from '../GetJson/GetJson.ts'
-import * as RemoveLeadingSlash from '../RemoveLeadingSlash/RemoveLeadingSlash.ts'
+import * as Rpc from '../Rpc/Rpc.ts'
 
 // TODO simplify code
 // 1. don't have playground prefix in fileMap json
 // 2. remove code here that removes the prefix
 export const searchFile = async (path: string): Promise<readonly string[]> => {
-  const fileList = await GetJson.getJson(FileMapUrl.fileMapUrl)
-  const result = fileList.map(RemoveLeadingSlash.removeLeadingSlash)
-  const prefixLength = path.length - 'file:///'.length
-  const final = []
-  for (const item of result) {
-    final.push(item.slice(prefixLength))
-  }
-  return final
+  return Rpc.invoke('ExtensionHost.searchFileWithFetch', path)
 }
