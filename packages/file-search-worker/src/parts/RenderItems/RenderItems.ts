df89018bc64cfa7712790bdcaf180e5f6160d1d7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-29 12:40:30 +0100

    feature: render full quickpick dom in worker when option is enabled (#290)

--- packages/file-search-worker/src/parts/RenderItems/RenderItems.ts
+++ packages/file-search-worker/src/parts/RenderItems/RenderItems.ts
@@ -1,7 +1,12 @@
 import type { QuickPickViewModel } from '../QuickPickViewModel/QuickPickViewModel.ts'
 import * as GetQuickPickItemsVirtualDom from '../GetQuickPickItemsVirtualDom/GetQuickPickItemsVirtualDom.ts'
+import * as GetQuickPickVirtualDom from '../GetQuickPickVirtualDom/GetQuickPickVirtualDom.ts'
 
 export const renderItems = (newState: QuickPickViewModel): readonly any[] => {
+  if (newState.renderAllItems) {
+    const dom = GetQuickPickVirtualDom.getQuickPickVirtualDom(newState.visibleItems)
+    return ['Viewlet.setDom2', dom]
+  }
   const dom = GetQuickPickItemsVirtualDom.getQuickPickItemsVirtualDom(newState.visibleItems)
   return ['Viewlet.send', newState.uid, /* method */ 'setItemsDom', dom]
 }
