a5a47085f58ed6be1837ba9ec0f9d9f6dd67d187
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-29 20:48:18 +0100

    feature: use new highlight function (#318)
    * feature: use new addHighlights function
    
    * lint

--- packages/file-search-worker/src/parts/GetQuickPickItemVirtualDom/GetQuickPickItemVirtualDom.ts
+++ packages/file-search-worker/src/parts/GetQuickPickItemVirtualDom/GetQuickPickItemVirtualDom.ts
@@ -1,45 +1,13 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
+import * as AddHighlights from '../AddHighlights/AddHighlights.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetFileIconVirtualDom from '../GetFileIconVirtualDom/GetFileIconVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
-const quickPickHighlight = {
-  type: VirtualDomElements.Span,
-  className: ClassNames.QuickPickHighlight,
-  childCount: 1,
-}
-
-const addHighlights = (dom: VirtualDomNode[], highlights: readonly any[], label: string): void => {
-  const labelDom = {
-    type: VirtualDomElements.Div,
-    className: ClassNames.QuickPickItemLabel,
-    childCount: 0,
-  }
-  dom.push(labelDom)
-  let position = 0
-  for (let i = 0; i < highlights.length; i += 2) {
-    const highlightStart = highlights[i]
-    const highlightEnd = highlights[i + 1]
-    if (position < highlightStart) {
-      const beforeText = label.slice(position, highlightStart)
-      labelDom.childCount++
-      dom.push(text(beforeText))
-    }
-    const highlightText = label.slice(highlightStart, highlightEnd)
-    labelDom.childCount++
-    dom.push(quickPickHighlight, text(highlightText))
-    position = highlightEnd
-  }
-  if (position < label.length) {
-    const afterText = label.slice(position)
-    labelDom.childCount++
-    dom.push(text(afterText))
-  }
-}
-
-export const getQuickPickItemVirtualDom = (visibleItem: any): readonly VirtualDomNode[] => {
+export const getQuickPickItemVirtualDom = (visibleItem: VisibleItem): readonly VirtualDomNode[] => {
   const { posInSet, label, setSize, isActive, description, icon, matches, fileIcon } = visibleItem
   const highlights = matches.slice(1)
   const dom: any[] = []
@@ -68,7 +36,7 @@ export const getQuickPickItemVirtualDom = (visibleItem: any): readonly VirtualDo
       childCount: 0,
     })
   }
-  addHighlights(dom, highlights, label)
+  AddHighlights.addHighlights(dom, highlights, label)
   if (description) {
     parent.childCount++
     dom.push(
