fab5c07983e312ab456e8ca73d0c6118ace16e85
Le Vivilet <levivlet@pm.me>
2025-03-29 09:00:43 +0100

    feature: simplify getSearchModule function

--- packages/file-search-worker/src/parts/SearchFile/SearchFile.ts
+++ packages/file-search-worker/src/parts/SearchFile/SearchFile.ts
@@ -4,7 +4,7 @@ import * as SearchFileModule from '../SearchFileModule/SearchFileModule.ts'
 export const searchFile = async (path: string, value: string, prepare: boolean, assetDir: string): Promise<readonly any[]> => {
   const protocol = GetProtocol.getProtocol(path)
   // TODO call different providers depending on protocol
-  const module = await SearchFileModule.getModule(protocol)
-  const result = await module.searchFile(path, value, prepare, assetDir)
+  const fn = await SearchFileModule.getModule(protocol)
+  const result = await fn(path, value, prepare, assetDir)
   return result
 }
