89c2963ed70528e300951f52973089a52108942c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-29 12:00:12 +0100

    feature: add function to create quickpick header dom (#275)
    * feature: add function to create quickpick header dom
    
    * role

---
+++ packages/file-search-worker/src/parts/GetQuickPickHeaderVirtualDom/GetQuickPickHeaderVirtualDom.ts
@@ -0,0 +1,23 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getQuickPickHeaderVirtualDom = (): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.QuickPickHeader,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Input,
+      className: ClassNames.InputBox,
+      spellcheck: false,
+      autocapitalize: 'off',
+      inputType: 'text',
+      childCount: 0,
+      role: AriaRoles.ComboBox,
+    },
+  ]
+}
