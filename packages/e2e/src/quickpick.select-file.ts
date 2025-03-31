c6aee5b84ba0f1ba3f69f27b7a7666d02201ffa0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-31 22:28:32 +0200

    feature: enable e2e tests (#341)
    * feature: enable e2e tests
    
    * skip failing test

--- packages/e2e/src/quickpick.select-file.ts
+++ packages/e2e/src/quickpick.select-file.ts
@@ -2,6 +2,8 @@ import { type Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'quickpick.select-file'
 
+export const skip = 1
+
 export const test: Test = async ({ FileSystem, Workspace, Main, Locator, expect, SideBar, QuickPick }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
