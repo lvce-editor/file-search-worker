cf189658d786acf0cd41496519d021eb30b3803b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-31 20:30:58 +0200

    feature: use i18n string for no results message (#339)

--- packages/file-search-worker/src/parts/GetQuickPickNoResultsVirtualDom/GetQuickPickNoResultsVirtualDom.ts
+++ packages/file-search-worker/src/parts/GetQuickPickNoResultsVirtualDom/GetQuickPickNoResultsVirtualDom.ts
@@ -1,9 +1,11 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as QuickPickStrings from '../QuickPickStrings/QuickPickStrings.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getQuickPickNoResultsVirtualDom = (): readonly VirtualDomNode[] => {
+  const noResults = QuickPickStrings.noResults()
   return [
     {
       type: VirtualDomElements.Div,
@@ -15,6 +17,6 @@ export const getQuickPickNoResultsVirtualDom = (): readonly VirtualDomNode[] => 
       className: ClassNames.Label,
       childCount: 1,
     },
-    text('No Results'),
+    text(noResults),
   ]
 }
