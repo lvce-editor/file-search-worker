8c6c24d0e0534d1e5f57a42dd442f94b00b88f2c
Le Vivilet <levivlet@pm.me>
2025-03-31 20:18:58 +0200

    feature: remove more unused code

--- packages/file-search-worker/src/parts/FileNotFoundError/FileNotFoundError.ts
+++
@@ -1,8 +0,0 @@
-export class FileNotFoundError extends Error {
-  code: string
-
-  constructor(uri: string) {
-    super(`File not found: ${uri}`)
-    this.code = 'ENOENT'
-  }
-}
--- packages/file-search-worker/src/parts/GetContentType/GetContentType.ts
+++
@@ -1,8 +0,0 @@
-import * as GetMimeType from '../GetMimeType/GetMimeType.ts'
-import * as Path from '../Path/Path.ts'
-
-export const getContentType = (uri: string): string => {
-  const extension = Path.extname(uri)
-  const mime = GetMimeType.getMimeType(extension)
-  return mime
-}
