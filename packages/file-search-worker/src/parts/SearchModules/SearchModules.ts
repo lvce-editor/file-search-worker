7ed28f15925cdc7f99e62f9856479a64d4f01c0e
Le Vivilet <levivlet@pm.me>
2025-03-31 20:03:09 +0200

    feature: use string type instead of any

--- packages/file-search-worker/src/parts/SearchModules/SearchModules.ts
+++ packages/file-search-worker/src/parts/SearchModules/SearchModules.ts
@@ -6,7 +6,7 @@ import * as SearchFileFetch from '../SearchFileWithFetch/SearchFileWithFetch.ts'
 import * as SearchFileHtml from '../SearchFileWithHtml/SearchFileWithHtml.ts'
 import * as SearchFileRipGrep from '../SearchFileWithRipGrep/SearchFileWithRipGrep.ts'
 
-export const searchModules: Record<any, SearchFileHandler> = {
+export const searchModules: Record<string, SearchFileHandler> = {
   [FileSystemProtocol.Memfs]: SearchFileMemfs.searchFile,
   [FileSystemProcotol.Fetch]: SearchFileFetch.searchFile,
   [FileSystemProtocol.Html]: SearchFileHtml.searchFile,
