35b3519ed49604e38c4deec57b7230e4a6cb570b
Le Vivilet <levivlet@pm.me>
2025-04-02 23:48:23 +0200

    feature: add getVisibleItems function

--- packages/file-search-worker/src/parts/QuickPickEntriesFile/QuickPickEntriesFile.ts
+++ packages/file-search-worker/src/parts/QuickPickEntriesFile/QuickPickEntriesFile.ts
@@ -1,11 +1,18 @@
+import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
+import * as FilterQuickPickItem from '../FilterQuickPickItem/FilterQuickPickItem.ts'
 import * as GetProtocol from '../GetProtocol/GetProtocol.ts'
 import * as OpenUri from '../OpenUri/OpenUri.ts'
 import * as QuickPickReturnValue from '../QuickPickReturnValue/QuickPickReturnValue.ts'
 import * as ViewletQuickPickStrings from '../QuickPickStrings/QuickPickStrings.ts'
+import * as RequestFileIcons from '../RequestFileIcons/RequestFileIcons.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
 import * as SearchFile from '../SearchFile/SearchFile.ts'
 import * as Workspace from '../Workspace/Workspace.ts'
+
+const state = {
+  items: [] as readonly any[],
+}
 
 const searchFile = async (path: any, value: any): Promise<readonly any[]> => {
   const prepare = true
@@ -37,7 +44,6 @@ export const getPicks = async (searchValue: any): Promise<readonly any[]> => {
     return []
   }
   const files = await searchFile(workspace, searchValue)
-  // const picks = files.map(toPick)
   return files
 }
 
@@ -103,4 +109,63 @@ export const isPrepared = (): boolean => {
   // TODO protocol should always be defined. For files it should use file protocol
   const protocol = GetProtocol.getProtocol(workspace)
   return !protocol
+}
+
+export const getVisibleItems = async (
+  minLineY: number,
+  maxLineY: number,
+  focusedIndex: number,
+  searchValue: string,
+): Promise<readonly VisibleItem[]> => {
+  // Query items using searchValue
+  const workspace = await Rpc.invoke('Workspace.getPath')
+  if (!workspace) {
+    return []
+  }
+  const files = await searchFile(workspace, searchValue)
+
+  // Filter items and store in state
+  const filteredItems = []
+  for (const file of files) {
+    const filterValue = getPickFilterValue(file)
+    const matches = FilterQuickPickItem.filterQuickPickItem(searchValue, filterValue)
+    if (matches.length > 0) {
+      filteredItems.push({
+        pick: file,
+        matches,
+      })
+    }
+  }
+  state.items = filteredItems
+
+  const visibleItems = []
+  const setSize = state.items.length
+  const max = Math.min(setSize, maxLineY)
+  const itemsToProcess = state.items.slice(minLineY, maxLineY)
+  const iconRequests = itemsToProcess.map((item) => ({
+    name: Workspace.pathBaseName(item.pick),
+    path: '',
+    type: DirentType.File,
+  }))
+  const icons = await RequestFileIcons.requestFileIcons(iconRequests)
+  let iconIndex = 0
+  for (let i = minLineY; i < max; i++) {
+    const item = state.items[i]
+    const pick = item.pick
+    const label = getPickLabel(pick)
+    const description = getPickDescription(pick)
+    const icon = getPickIcon()
+    const fileIcon = icons[iconIndex++]
+    visibleItems.push({
+      label,
+      description,
+      icon,
+      fileIcon,
+      posInSet: i + 1,
+      setSize,
+      isActive: i === focusedIndex,
+      matches: item.matches,
+    })
+  }
+  return visibleItems
 }
