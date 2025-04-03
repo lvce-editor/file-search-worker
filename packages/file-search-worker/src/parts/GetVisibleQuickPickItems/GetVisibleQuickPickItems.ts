7d92146a8aacf028579ddffb2422dd48b2f5edb0
Le Vivilet <levivlet@pm.me>
2025-04-03 23:46:33 +0200

    simplify code

--- packages/file-search-worker/src/parts/GetVisibleQuickPickItems/GetVisibleQuickPickItems.ts
+++ packages/file-search-worker/src/parts/GetVisibleQuickPickItems/GetVisibleQuickPickItems.ts
@@ -1,16 +1,16 @@
 import type { QuickPickEntriesModule } from '../QuickPickEntriesModule/QuickPickEntriesModule.ts'
 import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
 
-export const getVisible = async (
+export const getVisible = (
   provider: QuickPickEntriesModule,
   items: readonly any[],
   minLineY: number,
   maxLineY: number,
   focusedIndex: number,
   icons: readonly string[],
-): Promise<readonly VisibleItem[]> => {
+): readonly VisibleItem[] => {
   const setSize = items.length
   const range = items.slice(minLineY, maxLineY)
-  const visibleItems = await provider.getVisibleItems(range, minLineY, maxLineY, focusedIndex, setSize, icons)
+  const visibleItems = provider.getVisibleItems(range, minLineY, maxLineY, focusedIndex, setSize, icons)
   return visibleItems
 }
