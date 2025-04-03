545433960a598ac3035e4d9a8fc68dc8193b8bac
Le Vivilet <levivlet@pm.me>
2025-04-03 23:47:27 +0200

    simplify code

--- packages/file-search-worker/src/parts/Render2/Render2.ts
+++ packages/file-search-worker/src/parts/Render2/Render2.ts
@@ -1,7 +1,7 @@
 import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as QuickPickStates from '../QuickPickStates/QuickPickStates.ts'
 
-export const render2 = async (uid: number, diffResult: readonly number[]): Promise<readonly any[]> => {
+export const render2 = (uid: number, diffResult: readonly number[]): readonly any[] => {
   const { oldState, newState } = QuickPickStates.get(uid)
   QuickPickStates.set(uid, newState, newState)
   const commands = ApplyRender.applyRender(oldState, newState, diffResult)
