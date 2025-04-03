96392fc79c832a4c7ff2d87e541b73a9360b4a49
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-03 20:14:39 +0200

    feature: don't change state if element is already focused (#389)

--- packages/file-search-worker/src/parts/HandleFocus/HandleFocus.ts
+++ packages/file-search-worker/src/parts/HandleFocus/HandleFocus.ts
@@ -3,8 +3,14 @@ import * as Focus from '../Focus/Focus.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const handleFocus = async (state: QuickPickState): Promise<QuickPickState> => {
+  if (state.focused) {
+    return state
+  }
   // TODO fix virtual dom diffing so that input isn't destroyed and loses focus when rerendering
   await Focus.setFocus(WhenExpression.FocusQuickPickInput)
   // await CloseWidget.closeWidget(state.uid)
-  return state
+  return {
+    ...state,
+    focused: true,
+  }
 }
