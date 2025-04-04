44cc1c686e00975733c7a3e59aaf81e056faeea5
Le Vivilet <levivlet@pm.me>
2025-04-04 21:01:49 +0200

    feature: remove unused properties from quickpick modules

--- packages/file-search-worker/src/parts/QuickPickEntriesModule/QuickPickEntriesModule.ts
+++ packages/file-search-worker/src/parts/QuickPickEntriesModule/QuickPickEntriesModule.ts
@@ -1,20 +1,10 @@
-import type { Dirent } from '../Dirent/Dirent.ts'
 import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
 
 export interface QuickPickEntriesModule<T = any> {
-  readonly state: any
-  readonly name: string
   readonly getPlaceholder: () => string
   readonly getNoResults: () => string
   readonly getPicks: (value: string) => Promise<readonly T[]>
   readonly selectPick: (item: T) => Promise<any>
-  readonly getFilterValue: (item: T) => string
-  readonly getPickFilterValue: (item: T) => string
-  readonly getPickDescription: (item: T) => string
-  readonly getPickLabel: (item: T) => string
-  readonly getPickIcon: (item: T) => string
-  readonly getPickFileIcon?: (item: T) => Dirent
-  readonly isPrepared: () => boolean
   readonly getVisibleItems: (
     items: readonly T[],
     minLineY: number,
