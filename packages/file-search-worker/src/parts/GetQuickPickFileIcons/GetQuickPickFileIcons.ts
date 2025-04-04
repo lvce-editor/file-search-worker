c88eae39aa4c47c583bb7c4631a7d2015a122a36
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-04 21:07:12 +0200

    feature: move empty dirent into separate file (#437)

--- packages/file-search-worker/src/parts/GetQuickPickFileIcons/GetQuickPickFileIcons.ts
+++ packages/file-search-worker/src/parts/GetQuickPickFileIcons/GetQuickPickFileIcons.ts
@@ -1,16 +1,10 @@
-import type { Dirent } from '../Dirent/Dirent.ts'
 import type { FileIconCache } from '../FileIconCache/FileIconCache.ts'
 import type { QuickPickEntriesModule } from '../QuickPickEntriesModule/QuickPickEntriesModule.ts'
+import * as EmptyDirent from '../EmptyDirent/EmptyDirent.ts'
 import * as GetFileIconsCached from '../GetFileIconsCached/GetFileIconsCached.ts'
 import * as GetMissingIconRequests from '../GetMissingIconRequests/GetMissingIconRequests.ts'
 import * as RequestFileIcons from '../RequestFileIcons/RequestFileIcons.ts'
 import * as UpdateIconCache from '../UpdateIconCache/UpdateIconCache.ts'
-
-const emptyDirent: Dirent = {
-  name: '',
-  path: '',
-  type: 0,
-}
 
 export const getQuickPickFileIcons = async (
   provider: QuickPickEntriesModule,
@@ -21,7 +15,7 @@ export const getQuickPickFileIcons = async (
   newFileIconCache: FileIconCache
 }> => {
   const dirents = items.map((item) => {
-    const dirent = provider.getPickFileIcon?.(item) || emptyDirent
+    const dirent = provider.getPickFileIcon?.(item) || EmptyDirent.emptyDirent
     return dirent
   })
   const missingRequests = GetMissingIconRequests.getMissingIconRequests(dirents, fileIconCache)
