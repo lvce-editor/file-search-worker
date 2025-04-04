c6e30ce08ab0e2bbae4c229be5145c3bab7582f6
Le Vivilet <levivlet@pm.me>
2025-04-04 20:39:22 +0200

    feature: handle wrapCommand differently to better handle race conditions

--- packages/file-search-worker/src/parts/WrapCommand/WrapCommand.ts
+++ packages/file-search-worker/src/parts/WrapCommand/WrapCommand.ts
@@ -9,11 +9,7 @@ export const wrapCommand = (fn: any): any => {
       return
     }
     const latest = QuickPickStates.get(uid)
-    const merged: QuickPickState = {
-      ...latest.newState,
-      ...newerState,
-    }
-    QuickPickStates.set(uid, latest.newState, merged)
+    QuickPickStates.set(uid, latest.oldState, newerState)
   }
   return wrapped
 }
