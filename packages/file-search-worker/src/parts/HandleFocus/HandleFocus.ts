0308f3e7b65f8a20906a9cac54b98706ea3e3234
Le Vivilet <levivlet@pm.me>
2025-04-04 20:40:13 +0200

    feature: don't change state on focus change

--- packages/file-search-worker/src/parts/HandleFocus/HandleFocus.ts
+++ packages/file-search-worker/src/parts/HandleFocus/HandleFocus.ts
@@ -3,14 +3,8 @@ import * as Focus from '../Focus/Focus.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const handleFocus = async (state: QuickPickState): Promise<QuickPickState> => {
-  if (state.focused) {
-    return state
-  }
   // TODO fix virtual dom diffing so that input isn't destroyed and loses focus when rerendering
   await Focus.setFocus(WhenExpression.FocusQuickPickInput)
   // await CloseWidget.closeWidget(state.uid)
-  return {
-    ...state,
-    focused: true,
-  }
+  return state
 }
