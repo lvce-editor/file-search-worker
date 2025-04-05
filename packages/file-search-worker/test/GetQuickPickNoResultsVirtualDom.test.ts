415c54f353c38e4d84c61874e07dcc64a6f805ba
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-05 17:43:00 +0200

    feature: add no results dom test (#459)
    * feature: add no results dom test
    
    * lint

---
+++ packages/file-search-worker/test/GetQuickPickNoResultsVirtualDom.test.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@jest/globals'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as GetQuickPickNoResultsVirtualDom from '../src/parts/GetQuickPickNoResultsVirtualDom/GetQuickPickNoResultsVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+
+test('getQuickPickNoResultsVirtualDom', () => {
+  const result = GetQuickPickNoResultsVirtualDom.getQuickPickNoResultsVirtualDom()
+
+  expect(result.length).toBe(3)
+  expect(result[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: 'QuickPickItem QuickPickItemActive QuickPickStatus',
+    childCount: 1,
+  })
+  expect(result[1]).toEqual({
+    type: VirtualDomElements.Div,
+    className: ClassNames.Label,
+    childCount: 1,
+  })
+  expect(result[2].type).toBeDefined()
+  expect(typeof result[2].text).toBe('string')
+})
