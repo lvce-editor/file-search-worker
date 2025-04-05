a792e38183285e78a96818d69f5e50a5e5d949f3
Le Vivilet <levivlet@pm.me>
2025-04-05 09:03:58 +0200

    feature: move noop items into separate file

---
+++ packages/file-search-worker/src/parts/GetVisibleItemsQuickPickEntriesNoop/GetVisibleItemsQuickPickEntriesNoop.ts
@@ -0,0 +1,12 @@
+import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
+
+export const getVisibleItems = (
+  picks: readonly any[],
+  minLineY: number,
+  maxLineY: number,
+  focusedIndex: number,
+  setSize: number,
+  icons: readonly string[],
+): readonly VisibleItem[] => {
+  return []
+}
