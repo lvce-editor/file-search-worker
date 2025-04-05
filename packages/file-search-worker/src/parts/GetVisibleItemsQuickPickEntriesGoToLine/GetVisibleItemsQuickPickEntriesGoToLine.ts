76586577103907c6891d623cfe1c4f3e9091c24a
Le Vivilet <levivlet@pm.me>
2025-04-05 09:02:21 +0200

    feature: move go to line items into separate file

---
+++ packages/file-search-worker/src/parts/GetVisibleItemsQuickPickEntriesGoToLine/GetVisibleItemsQuickPickEntriesGoToLine.ts
@@ -0,0 +1,22 @@
+import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
+
+export const getVisibleItems = (
+  picks: readonly any[],
+  minLineY: number,
+  maxLineY: number,
+  focusedIndex: number,
+  setSize: number,
+  icons: readonly string[],
+): readonly VisibleItem[] => {
+  const visibleItems = picks.map((pick, index) => ({
+    description: '',
+    fileIcon: '',
+    icon: '',
+    isActive: index + minLineY === focusedIndex,
+    label: pick.label,
+    matches: [],
+    posInSet: index + minLineY + 1,
+    setSize,
+  }))
+  return visibleItems
+}
--- packages/file-search-worker/src/parts/QuickPickEntriesGoToLine/QuickPickEntriesGoToLine.ts
+++ packages/file-search-worker/src/parts/QuickPickEntriesGoToLine/QuickPickEntriesGoToLine.ts
@@ -1,5 +1,5 @@
-import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
 import * as Command from '../Command/Command.ts'
+import * as GetVisibleItemsQuickPickEntriesGoToLine from '../GetVisibleItemsQuickPickEntriesGoToLine/GetVisibleItemsQuickPickEntriesGoToLine.ts'
 import * as QuickPickReturnValue from '../QuickPickReturnValue/QuickPickReturnValue.ts'
 
 export const name = 'goToLine'
@@ -79,23 +79,4 @@ export const getFilterValue = (value: any): string => {
   return value
 }
 
-export const getVisibleItems = (
-  picks: readonly any[],
-  minLineY: number,
-  maxLineY: number,
-  focusedIndex: number,
-  setSize: number,
-  icons: readonly string[],
-): readonly VisibleItem[] => {
-  const visibleItems = picks.map((pick, index) => ({
-    description: '',
-    fileIcon: '',
-    icon: '',
-    isActive: index + minLineY === focusedIndex,
-    label: pick.label,
-    matches: [],
-    posInSet: index + minLineY + 1,
-    setSize,
-  }))
-  return visibleItems
-}
+export const getVisibleItems = GetVisibleItemsQuickPickEntriesGoToLine.getVisibleItems
