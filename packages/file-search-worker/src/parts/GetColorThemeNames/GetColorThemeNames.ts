04032f7dde4f011fd93f67e36c496edbaf3dbdc3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-05 19:48:24 +0200

    feature: add color theme types (#474)

--- packages/file-search-worker/src/parts/GetColorThemeNames/GetColorThemeNames.ts
+++ packages/file-search-worker/src/parts/GetColorThemeNames/GetColorThemeNames.ts
@@ -1,5 +1,5 @@
 import * as Rpc from '../Rpc/Rpc.ts'
 
-export const getColorThemeNames = async (): Promise<readonly any[]> => {
+export const getColorThemeNames = async (): Promise<readonly string[]> => {
   return Rpc.invoke(/* Ajax.getJson */ 'ColorTheme.getColorThemeNames')
 }
