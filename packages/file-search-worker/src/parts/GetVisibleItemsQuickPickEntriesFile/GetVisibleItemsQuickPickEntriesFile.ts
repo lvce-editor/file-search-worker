1cb1f117e056a00254ce5ae76c45697ddfff7fe7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-05 08:53:02 +0200

    feature: move visible items into separate file (#438)

---
+++ packages/file-search-worker/src/parts/GetVisibleItemsQuickPickEntriesFile/GetVisibleItemsQuickPickEntriesFile.ts
@@ -0,0 +1,50 @@
+import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
+import * as Workspace from '../Workspace/Workspace.ts'
+
+export const getVisibleItems = (
+  files: readonly any[],
+  minLineY: number,
+  maxLineY: number,
+  focusedIndex: number,
+  setSize: number,
+  icons: readonly string[],
+): readonly VisibleItem[] => {
+  const visibleItems = files.map((item, i) => {
+    const pick = item.pick
+    const label = getPickLabel(pick)
+    const description = getPickDescription(pick)
+    const icon = getPickIcon()
+    const fileIcon = icons[i]
+    return {
+      label,
+      description,
+      icon,
+      fileIcon,
+      posInSet: minLineY + i + 1,
+      setSize,
+      isActive: i === focusedIndex,
+      matches: item.matches,
+    }
+  })
+  return visibleItems
+}
+
+const getPickLabel = (pick: any): string => {
+  if (typeof pick === 'object') {
+    pick = pick.pick
+  }
+  const baseName = Workspace.pathBaseName(pick)
+  return baseName
+}
+
+const getPickDescription = (pick: any): string => {
+  if (typeof pick === 'object') {
+    pick = pick.pick
+  }
+  const dirName = Workspace.pathDirName(pick)
+  return dirName
+}
+
+const getPickIcon = (): string => {
+  return ''
+}
