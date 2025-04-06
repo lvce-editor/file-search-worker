773528882b5c2af023d2dc0065c0cff109e34641
Le Vivilet <levivlet@pm.me>
2025-04-06 13:25:20 +0200

    feature: use getHighlights function

--- packages/file-search-worker/src/parts/GetHighlights/GetHighlights.ts
+++ packages/file-search-worker/src/parts/GetHighlights/GetHighlights.ts
@@ -1,5 +1,6 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import { getHighlightSections } from '../GetHighlightSections/GetHighlightSections.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
@@ -16,24 +17,20 @@ export const getHighlights = (highlights: readonly number[], label: string): rea
     childCount: 0,
   }
   const nodes: VirtualDomNode[] = [labelDom]
-  let position = 0
-  for (let i = 0; i < highlights.length; i += 2) {
-    const highlightStart = highlights[i]
-    const highlightEnd = highlights[i + 1]
-    if (position < highlightStart) {
-      const beforeText = label.slice(position, highlightStart)
-      labelDom.childCount++
-      nodes.push(text(beforeText))
+  const sections = getHighlightSections(highlights, label)
+  if (sections.length === 0) {
+    labelDom.childCount++
+    nodes.push(text(label))
+  } else {
+    for (const section of sections) {
+      if (section.highlighted) {
+        labelDom.childCount++
+        nodes.push(quickPickHighlight, text(section.text))
+      } else {
+        labelDom.childCount++
+        nodes.push(text(section.text))
+      }
     }
-    const highlightText = label.slice(highlightStart, highlightEnd)
-    labelDom.childCount++
-    nodes.push(quickPickHighlight, text(highlightText))
-    position = highlightEnd
-  }
-  if (position < label.length) {
-    const afterText = label.slice(position)
-    labelDom.childCount++
-    nodes.push(text(afterText))
   }
   return nodes
 }
