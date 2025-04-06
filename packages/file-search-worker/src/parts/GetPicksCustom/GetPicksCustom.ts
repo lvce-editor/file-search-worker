a608408e5c8314a68b80f50e8dfdd4a8d31f3da6
Le Vivilet <levivlet@pm.me>
2025-04-06 16:00:41 +0200

    fix dependency

--- packages/file-search-worker/src/parts/GetPicksCustom/GetPicksCustom.ts
+++ packages/file-search-worker/src/parts/GetPicksCustom/GetPicksCustom.ts
@@ -1,5 +1,5 @@
 import type { ProtoVisibleItem } from '../ProtoVisibleItem/ProtoVisibleItem.ts'
-import { state } from '../QuickPickEntriesCustom/QuickPickEntriesCustom.ts'
+import { state } from '../QuickPickEntriesCustom/QuickPickEntriesCustomState.ts'
 
 export const getPicks = async (searchValue: string): Promise<readonly ProtoVisibleItem[]> => {
   const items = state.args[1] || []
