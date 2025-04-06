9ff698923e2d0497edcaf797e0794b99490d727d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-06 18:10:54 +0200

    feature: remove unused render file (#542)

--- packages/file-search-worker/src/parts/Render/Render.ts
+++
@@ -1,9 +0,0 @@
-import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
-import * as Diff from '../Diff/Diff.ts'
-import * as QuickPickStates from '../QuickPickStates/QuickPickStates.ts'
-
-export const doRender = (uid: number): readonly any[] => {
-  const { oldState, newState } = QuickPickStates.get(uid)
-  const diffResult = Diff.diff(oldState, newState)
-  return ApplyRender.applyRender(oldState, newState, diffResult)
-}
