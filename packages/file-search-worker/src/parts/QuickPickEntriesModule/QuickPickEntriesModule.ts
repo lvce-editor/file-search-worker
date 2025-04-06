436fb044df0d447a68bdf6677c2bd9109a3265a1
Le Vivilet <levivlet@pm.me>
2025-04-06 14:08:06 +0200

    feature: add select type

--- packages/file-search-worker/src/parts/QuickPickEntriesModule/QuickPickEntriesModule.ts
+++ packages/file-search-worker/src/parts/QuickPickEntriesModule/QuickPickEntriesModule.ts
@@ -1,10 +1,11 @@
 import type { Dirent } from '../Dirent/Dirent.ts'
 import type { ProtoVisibleItem } from '../ProtoVisibleItem/ProtoVisibleItem.ts'
+import type { SelectPickResult } from '../SelectPickRresult/SelectPickResult.ts'
 
 export interface QuickPickEntriesModule {
   readonly state: any
   readonly getPicks: (value: string) => Promise<readonly ProtoVisibleItem[]>
-  readonly selectPick: (item: ProtoVisibleItem) => Promise<any>
+  readonly selectPick: (item: ProtoVisibleItem) => Promise<SelectPickResult>
   readonly getPickFileIcon?: (item: ProtoVisibleItem) => Dirent
   readonly getVisibleItems: (items: readonly ProtoVisibleItem[], icons: readonly string[]) => readonly ProtoVisibleItem[]
 }
