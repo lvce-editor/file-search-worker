26369fc80c312a0a084faee299f35dff11c16a8d
Le Vivilet <levivlet@pm.me>
2025-04-06 22:11:31 +0200

    feature: improve help picks

--- packages/file-search-worker/src/parts/GetPicksHelp/GetPicksHelp.ts
+++ packages/file-search-worker/src/parts/GetPicksHelp/GetPicksHelp.ts
@@ -1,8 +1,53 @@
 import type { ProtoVisibleItem } from '../ProtoVisibleItem/ProtoVisibleItem.ts'
+import * as DirentType from '../DirentType/DirentType.ts'
+import * as QuickPickStrings from '../QuickPickStrings/QuickPickStrings.ts'
 
 export const getPicks = async (): Promise<readonly ProtoVisibleItem[]> => {
-  // const views = ViewService.getViews()
-  // const picks = views.map(toPick)
-  // return picks
-  return []
+  return [
+    {
+      description: QuickPickStrings.goToFile(),
+      direntType: DirentType.None,
+      fileIcon: '',
+      icon: '',
+      label: '...',
+      matches: [],
+      uri: '',
+    },
+    {
+      description: QuickPickStrings.goToLineColumn(),
+      direntType: DirentType.None,
+      fileIcon: '',
+      icon: '',
+      label: ':',
+      matches: [],
+      uri: '',
+    },
+    {
+      description: QuickPickStrings.goToSymbolInEditor(),
+      direntType: DirentType.None,
+      fileIcon: '',
+      icon: '',
+      label: ':',
+      matches: [],
+      uri: '',
+    },
+    {
+      description: QuickPickStrings.searchForText(),
+      direntType: DirentType.None,
+      fileIcon: '',
+      icon: '',
+      label: '%',
+      matches: [],
+      uri: '',
+    },
+    {
+      description: QuickPickStrings.showAndRunCommands(),
+      direntType: DirentType.None,
+      fileIcon: '',
+      icon: '',
+      label: '>',
+      matches: [],
+      uri: '',
+    },
+  ]
 }
