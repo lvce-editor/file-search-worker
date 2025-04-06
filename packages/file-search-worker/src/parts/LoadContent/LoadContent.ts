71bf1d5d4c80a47db8f2c940eabd50a60d8defb3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-06 17:34:32 +0200

    Discard changes to packages/file-search-worker/src/parts/LoadContent/LoadContent.ts

--- packages/file-search-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/file-search-worker/src/parts/LoadContent/LoadContent.ts
@@ -2,7 +2,6 @@ import type { QuickPickState } from '../QuickPickState/QuickPickState.ts'
 import * as Assert from '../Assert/Assert.ts'
 import * as FilterQuickPickItems from '../FilterQuickPickItems/FilterQuickPickItems.ts'
 import * as GetDefaultValue from '../GetDefaultValue/GetDefaultValue.ts'
-import * as GetFilterValue from '../GetFilterValue/GetFilterValue.ts'
 import * as GetPicks from '../GetPicks/GetPicks.ts'
 import * as GetQuickPickFileIcons from '../GetQuickPickFileIcons/GetQuickPickFileIcons.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
@@ -20,7 +19,8 @@ export const loadContent = async (state: QuickPickState): Promise<QuickPickState
   }
   const newPicks = await GetPicks.getPicks(uri, value)
   Assert.array(newPicks)
-  const filterValue = GetFilterValue.getFilterValue(uri, value)
+  // @ts-ignore
+  const filterValue = provider.getFilterValue(value)
   const items = FilterQuickPickItems.filterQuickPickItems(newPicks, filterValue)
   const minLineY = 0
   const maxLineY = Math.min(minLineY + state.maxVisibleItems, newPicks.length)
