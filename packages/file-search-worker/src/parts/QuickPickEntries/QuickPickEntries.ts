badd791036f33b8eef9e7e9ea91265409d404142
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-06 16:03:01 +0200

    feature: improve quickpick registry (#519)
    * feature: improve quickpick registry
    
    * keep old code
    
    * fix dependency

--- packages/file-search-worker/src/parts/QuickPickEntries/QuickPickEntries.ts
+++ packages/file-search-worker/src/parts/QuickPickEntries/QuickPickEntries.ts
@@ -2,10 +2,23 @@ import type { QuickPickEntriesModule } from '../QuickPickEntriesModule/QuickPick
 
 const state: Record<string, QuickPickEntriesModule> = Object.create(null)
 
+const select: Record<string, QuickPickEntriesModule['selectPick']> = Object.create(null)
+
+const getPick: Record<string, QuickPickEntriesModule['getPicks']> = Object.create(null)
+
 export const register = (modules: Record<string, QuickPickEntriesModule>): void => {
   Object.assign(state, modules)
 }
 
+export const registerSelect = (modules: Record<string, QuickPickEntriesModule['selectPick']>): void => {
+  Object.assign(select, modules)
+}
+
+export const registerGetPick = (modules: Record<string, QuickPickEntriesModule['getPicks']>): void => {
+  Object.assign(getPick, modules)
+}
+
+// deprecated
 export const get = (moduleId: string): QuickPickEntriesModule => {
   const module = state[moduleId]
   if (!module) {
@@ -22,4 +35,14 @@ const getProperty = <T extends keyof QuickPickEntriesModule>(moduleId: string, p
 
 export const getPicks = (id: string): QuickPickEntriesModule['getPicks'] => {
   return getProperty(id, 'getPicks')
+}
+
+// export const getPicks = (id: string): QuickPickEntriesModule['getPicks'] => {
+//   const fn = getPick[id]
+//   return fn
+// }
+
+export const getSelect = (id: string): QuickPickEntriesModule['selectPick'] => {
+  const fn = select[id]
+  return fn
 }
