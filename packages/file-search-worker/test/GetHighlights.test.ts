27e26d47e1330b4b5324f4b0710b938c4e79046b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-06 13:23:04 +0200

    feature: add getHighlights function (#498)
    * feature: add getHighlights function
    
    * add test
    
    * lint

---
+++ packages/file-search-worker/test/GetHighlights.test.ts
@@ -0,0 +1,109 @@
+import { test, expect } from '@jest/globals'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as GetHighlights from '../src/parts/GetHighlights/GetHighlights.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+
+test('getHighlights - empty highlights', () => {
+  const result = GetHighlights.getHighlights([], 'test')
+  expect(result).toHaveLength(2)
+  expect(result[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: ClassNames.QuickPickItemLabel,
+    childCount: 1,
+  })
+  expect(result[1]).toEqual({
+    type: 12,
+    text: 'test',
+    childCount: 0,
+  })
+})
+
+test('getHighlights - single highlight', () => {
+  const result = GetHighlights.getHighlights([1, 3], 'test')
+  expect(result).toHaveLength(5)
+  expect(result[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: ClassNames.QuickPickItemLabel,
+    childCount: 3,
+  })
+  expect(result[1]).toEqual({
+    type: 12,
+    text: 't',
+    childCount: 0,
+  })
+  expect(result[2]).toEqual({
+    type: VirtualDomElements.Span,
+    className: ClassNames.QuickPickHighlight,
+    childCount: 1,
+  })
+  expect(result[3]).toEqual({
+    type: 12,
+    text: 'es',
+    childCount: 0,
+  })
+  expect(result[4]).toEqual({
+    type: 12,
+    text: 't',
+    childCount: 0,
+  })
+})
+
+test('getHighlights - multiple highlights', () => {
+  const result = GetHighlights.getHighlights([1, 3, 5, 7], 'testtest')
+  expect(result).toHaveLength(8)
+  expect(result[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: ClassNames.QuickPickItemLabel,
+    childCount: 5,
+  })
+})
+
+test('getHighlights - highlight at start', () => {
+  const result = GetHighlights.getHighlights([0, 2], 'test')
+  expect(result).toHaveLength(4)
+  expect(result[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: ClassNames.QuickPickItemLabel,
+    childCount: 2,
+  })
+  expect(result[1]).toEqual({
+    type: VirtualDomElements.Span,
+    className: ClassNames.QuickPickHighlight,
+    childCount: 1,
+  })
+  expect(result[2]).toEqual({
+    type: 12,
+    text: 'te',
+    childCount: 0,
+  })
+  expect(result[3]).toEqual({
+    type: 12,
+    text: 'st',
+    childCount: 0,
+  })
+})
+
+test('getHighlights - highlight at end', () => {
+  const result = GetHighlights.getHighlights([2, 4], 'test')
+  expect(result).toHaveLength(4)
+  expect(result[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: ClassNames.QuickPickItemLabel,
+    childCount: 2,
+  })
+  expect(result[1]).toEqual({
+    type: 12,
+    text: 'te',
+    childCount: 0,
+  })
+  expect(result[2]).toEqual({
+    type: VirtualDomElements.Span,
+    className: ClassNames.QuickPickHighlight,
+    childCount: 1,
+  })
+  expect(result[3]).toEqual({
+    type: 12,
+    text: 'st',
+    childCount: 0,
+  })
+})
