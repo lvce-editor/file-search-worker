9bdec64ac6f6ea750ec2ba58d7245ee99ff38aa9
Le Vivilet <levivlet@pm.me>
2025-04-06 17:46:39 +0200

    feature: remove unused code

--- packages/file-search-worker/src/parts/QuickPickEntriesModules/QuickPickEntriesModules.ts
+++ packages/file-search-worker/src/parts/QuickPickEntriesModules/QuickPickEntriesModules.ts
@@ -1,5 +1,4 @@
 import type { GetPicks } from '../GetPicks/GetPicks.ts'
-import type { QuickPickEntriesModule } from '../QuickPickEntriesModule/QuickPickEntriesModule.ts'
 import type { SelectPick } from '../SelectPick/SelectPick.ts'
 import * as GetPicksColorTheme from '../GetPicksColorTheme/GetPicksColorTheme.ts'
 import * as GetPicksCommand from '../GetPicksCommand/GetPicksCommand.ts'
@@ -10,15 +9,6 @@ import * as GetPicksOpenRecent from '../GetPicksOpenRecent/GetPicksOpenRecent.ts
 import * as GetPicksSymbol from '../GetPicksSymbol/GetPicksSymbol.ts'
 import * as GetPicksView from '../GetPicksView/GetPicksView.ts'
 import * as GetPicksWorkspaceSymbol from '../GetPicksWorkspaceSymbol/GetPicksWorkspaceSymbol.ts'
-import * as QuickPickEntriesColorTheme from '../QuickPickEntriesColorTheme/QuickPickEntriesColorTheme.ts'
-import * as QuickPickEntriesCommand from '../QuickPickEntriesCommand/QuickPickEntriesCommand.ts'
-import * as QuickPickEntriesCustom from '../QuickPickEntriesCustom/QuickPickEntriesCustom.ts'
-import * as QuickPickEntriesEverything from '../QuickPickEntriesEverything/QuickPickEntriesEverything.ts'
-import * as QuickPickEntriesFile from '../QuickPickEntriesFile/QuickPickEntriesFile.ts'
-import * as QuickPickEntriesOpenRecent from '../QuickPickEntriesOpenRecent/QuickPickEntriesOpenRecent.ts'
-import * as QuickPickEntriesSymbol from '../QuickPickEntriesSymbol/QuickPickEntriesSymbol.ts'
-import * as QuickPickEntriesView from '../QuickPickEntriesView/QuickPickEntriesView.ts'
-import * as QuickPickEntriesWorkspaceSymbol from '../QuickPickEntriesWorkspaceSymbol/QuickPickEntriesWorkspaceSymbol.ts'
 import * as QuickPickEntryId from '../QuickPickEntryId/QuickPickEntryId.ts'
 import * as SelectPickColorTheme from '../SelectPickColorTheme/SelectPickColorTheme.ts'
 import * as SelectPickCommand from '../SelectPickCommand/SelectPickCommand.ts'
@@ -28,22 +18,6 @@ import * as SelectPickFile from '../SelectPickFile/SelectPickFile.ts'
 import * as SelectPickRecent from '../SelectPickRecent/SelectPickRecent.ts'
 import * as SelectPickSymbol from '../SelectPickSymbol/SelectPickSymbol.ts'
 import * as SelectPickWorkspaceSymbol from '../SelectPickWorkspaceSymbol/SelectPickWorkspaceSymbol.ts'
-
-/**
- * deprecated
- */
-export const quickPickEntriesModules: Record<string, QuickPickEntriesModule> = {
-  [QuickPickEntryId.ColorTheme]: QuickPickEntriesColorTheme,
-  [QuickPickEntryId.CommandPalette]: QuickPickEntriesEverything,
-  [QuickPickEntryId.Commands]: QuickPickEntriesCommand,
-  [QuickPickEntryId.Custom]: QuickPickEntriesCustom,
-  [QuickPickEntryId.EveryThing]: QuickPickEntriesEverything,
-  [QuickPickEntryId.File]: QuickPickEntriesFile,
-  [QuickPickEntryId.Recent]: QuickPickEntriesOpenRecent,
-  [QuickPickEntryId.Symbol]: QuickPickEntriesSymbol,
-  [QuickPickEntryId.View]: QuickPickEntriesView,
-  [QuickPickEntryId.WorkspaceSymbol]: QuickPickEntriesWorkspaceSymbol,
-}
 
 export const selectPicks: Record<string, SelectPick> = {
   [QuickPickEntryId.ColorTheme]: SelectPickColorTheme.selectPick,
