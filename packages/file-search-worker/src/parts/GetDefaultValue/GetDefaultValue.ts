3334520907d81adaa9fde4b78a0b7772b0a7db55
Le Vivilet <levivlet@pm.me>
2025-04-06 22:20:03 +0200

    feature: use id enum

--- packages/file-search-worker/src/parts/GetDefaultValue/GetDefaultValue.ts
+++ packages/file-search-worker/src/parts/GetDefaultValue/GetDefaultValue.ts
@@ -1,7 +1,10 @@
+import * as QuickPickEntryId from '../QuickPickEntryId/QuickPickEntryId.ts'
+import * as QuickPickPrefix from '../QuickPickPrefix/QuickPickPrefix.ts'
+
 export const getDefaultValue = (uri: string): string => {
   switch (uri) {
-    case 'quickPick://everything':
-      return '>'
+    case QuickPickEntryId.EveryThing:
+      return QuickPickPrefix.Command
     default:
       return ''
   }
