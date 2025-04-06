04381c773254874f75946f0d3c06eee3e721b317
Le Vivilet <levivlet@pm.me>
2025-04-06 20:25:51 +0200

    feature: use classnames enum

--- packages/file-search-worker/src/parts/GetQuickPickNoResultsVirtualDom/GetQuickPickNoResultsVirtualDom.ts
+++ packages/file-search-worker/src/parts/GetQuickPickNoResultsVirtualDom/GetQuickPickNoResultsVirtualDom.ts
@@ -1,5 +1,6 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as QuickPickStrings from '../QuickPickStrings/QuickPickStrings.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
@@ -9,7 +10,7 @@ export const getQuickPickNoResultsVirtualDom = (): readonly VirtualDomNode[] => 
   return [
     {
       type: VirtualDomElements.Div,
-      className: 'QuickPickItem QuickPickItemActive QuickPickStatus',
+      className: MergeClassNames.mergeClassNames(ClassNames.QuickPickItem, ClassNames.QuickPickItemActive, ClassNames.QuickPickStatus),
       childCount: 1,
     },
     {
