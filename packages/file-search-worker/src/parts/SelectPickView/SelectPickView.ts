2c9be99a882422eade583e72158b54a7f15345f6
Le Vivilet <levivlet@pm.me>
2025-04-06 09:33:39 +0200

    feature: move selectPickView into separate file

---
+++ packages/file-search-worker/src/parts/SelectPickView/SelectPickView.ts
@@ -0,0 +1,6 @@
+export const selectPick = async (item: any): Promise<any> => {
+  // Command.execute(/* openView */ 549, /* viewName */ item.label)
+  // return {
+  //   command: QuickPickReturnValue.Hide,
+  // }
+}
