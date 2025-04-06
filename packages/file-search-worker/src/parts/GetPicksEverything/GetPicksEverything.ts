5612730090bd0f4de5383db58a95bcf123f3fb7c
Le Vivilet <levivlet@pm.me>
2025-04-06 16:12:52 +0200

    feature: simplify quickPickEverything state

--- packages/file-search-worker/src/parts/GetPicksEverything/GetPicksEverything.ts
+++ packages/file-search-worker/src/parts/GetPicksEverything/GetPicksEverything.ts
@@ -3,7 +3,6 @@ import * as GetPicks from '../GetPicks/GetPicks.ts'
 import * as GetQuickPickPrefix from '../GetQuickPickPrefix/GetQuickPickPrefix.ts'
 import * as GetQuickPickProviderId from '../GetQuickPickProviderId/GetQuickPickProviderId.ts'
 import { state } from '../GetVisibleItemsQuickPickEntriesEverything/GetVisibleItemsQuickPickEntriesEverything.ts'
-import * as QuickPickEntries from '../QuickPickEntries/QuickPickEntries.ts'
 
 export const getPicks = async (value: string): Promise<readonly ProtoVisibleItem[]> => {
   const prefix = GetQuickPickPrefix.getQuickPickPrefix(value)
@@ -12,8 +11,7 @@ export const getPicks = async (value: string): Promise<readonly ProtoVisibleItem
   // TODO race condition
   if (state.prefix !== prefix) {
     state.prefix = prefix
-    const provider = QuickPickEntries.get(providerId)
-    state.provider = provider
+    state.provider = providerId
   }
   // TODO this line is a bit duplicated with getFilterValue
   const slicedValue = value.slice(prefix.length).trimStart()
--- packages/file-search-worker/src/parts/SelectPickEverything/SelectPickEverything.ts
+++ packages/file-search-worker/src/parts/SelectPickEverything/SelectPickEverything.ts
@@ -1,7 +1,10 @@
 import type { ProtoVisibleItem } from '../ProtoVisibleItem/ProtoVisibleItem.ts'
+import type { SelectPickResult } from '../SelectPickRresult/SelectPickResult.ts'
 import { state } from '../GetVisibleItemsQuickPickEntriesEverything/GetVisibleItemsQuickPickEntriesEverything.ts'
+import * as QuickPickEntries from '../QuickPickEntries/QuickPickEntries.ts'
 
-export const selectPick = (item: ProtoVisibleItem): Promise<any> => {
+export const selectPick = (item: ProtoVisibleItem): Promise<SelectPickResult> => {
   const { provider } = state
-  return provider.selectPick(item)
+  const fn = QuickPickEntries.getSelect(provider)
+  return fn(item)
 }
