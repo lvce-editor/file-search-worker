28934f05f2293976563ab4dec09fa3c24c0c74b4
Le Vivilet <levivlet@pm.me>
2025-04-06 18:10:15 +0200

    feature: remove unused files

--- packages/file-search-worker/src/parts/EmptyDirent/EmptyDirent.ts
+++
@@ -1,7 +0,0 @@
-import type { Dirent } from '../Dirent/Dirent.ts'
-
-export const emptyDirent: Dirent = {
-  name: '',
-  path: '',
-  type: 0,
-}
--- packages/file-search-worker/src/parts/GetBaseName/GetBaseName.ts
+++
@@ -1,3 +0,0 @@
-export const getBaseName = (path: string): string => {
-  return path.slice(path.lastIndexOf('/') + 1)
-}
