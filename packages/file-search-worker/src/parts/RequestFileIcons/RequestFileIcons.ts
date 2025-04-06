8010827d1c4fe50320a1d0cf41ac000f95164593
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-06 18:34:16 +0200

    feature: move requestIcon into separate function (#549)

--- packages/file-search-worker/src/parts/RequestFileIcons/RequestFileIcons.ts
+++ packages/file-search-worker/src/parts/RequestFileIcons/RequestFileIcons.ts
@@ -2,14 +2,16 @@ import type { IconRequest } from '../IconRequest/IconRequest.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
 
+const requestFileIcon = async (request: IconRequest): Promise<string> => {
+  if (!request.name) {
+    return ''
+  }
+  return request.type === DirentType.File
+    ? Rpc.invoke('IconTheme.getFileIcon', { name: request.name })
+    : Rpc.invoke('IconTheme.getFolderIcon', { name: request.name })
+}
+
 export const requestFileIcons = async (requests: readonly IconRequest[]): Promise<readonly string[]> => {
-  const promises = requests.map((request) => {
-    if (!request.name) {
-      return ''
-    }
-    return request.type === DirentType.File
-      ? Rpc.invoke('IconTheme.getFileIcon', { name: request.name })
-      : Rpc.invoke('IconTheme.getFolderIcon', { name: request.name })
-  })
+  const promises = requests.map(requestFileIcon)
   return Promise.all(promises)
 }
