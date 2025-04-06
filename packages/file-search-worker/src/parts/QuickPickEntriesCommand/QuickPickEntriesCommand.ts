afd50d422adbaf3c4bfd57f36b22f3336c1dd417
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-06 09:34:03 +0200

    feature: move selectPick command into separate file (#484)
    * feature: move selectPick command into separate file
    
    * lint

--- packages/file-search-worker/src/parts/QuickPickEntriesCommand/QuickPickEntriesCommand.ts
+++ packages/file-search-worker/src/parts/QuickPickEntriesCommand/QuickPickEntriesCommand.ts
@@ -1,8 +1,5 @@
-import * as ErrorHandling from '../ErrorHandling/ErrorHandling.ts'
 import * as GetVisibleItemsQuickPickEntriesCommand from '../GetVisibleItemsQuickPickEntriesCommand/GetVisibleItemsQuickPickEntriesCommand.ts'
-import * as QuickPickReturnValue from '../QuickPickReturnValue/QuickPickReturnValue.ts'
 import * as ViewletQuickPickStrings from '../QuickPickStrings/QuickPickStrings.ts'
-import * as Rpc from '../Rpc/Rpc.ts'
 
 export const name = 'command'
 
@@ -32,47 +29,6 @@ export const getNoResults = (): any => {
 
 export const getVisibleItems = GetVisibleItemsQuickPickEntriesCommand.getVisibleItems
 
-const shouldHide = (item: any): boolean => {
-  if (item.id === 'Viewlet.openWidget' && item.args[0] === 'QuickPick') {
-    return false
-  }
-  return true
-}
-
-const selectPickBuiltin = async (item: any): Promise<any> => {
-  const args = item.args || []
-  // TODO ids should be all numbers for efficiency -> also directly can call command
-  await Rpc.invoke(item.id, ...args)
-  if (shouldHide(item)) {
-    return {
-      command: QuickPickReturnValue.Hide,
-    }
-  }
-  return {
-    command: QuickPickReturnValue.KeepOpen,
-  }
-}
-
-const selectPickExtension = async (item: any): Promise<any> => {
-  const id = item.id.slice(4) // TODO lots of string allocation with 'ext.' find a better way to separate builtin commands from extension commands
-  try {
-    await Rpc.invoke('ExtensionHost.executeCommand', id)
-  } catch (error) {
-    await ErrorHandling.handleError(error, false)
-    await ErrorHandling.showErrorDialog(error)
-  }
-  return {
-    command: QuickPickReturnValue.Hide,
-  }
-}
-
-export const selectPick = async (item: any): Promise<any> => {
-  if (item.id.startsWith('ext.')) {
-    return selectPickExtension(item)
-  }
-  return selectPickBuiltin(item)
-}
-
 export const getFilterValue = (value: string): string => {
   return value.trim()
 }
@@ -90,3 +46,5 @@ export const getPickIcon = (): string => {
 }
 
 export { getPicks } from '../GetPicksCommand/GetPicksCommand.ts'
+
+export { selectPick } from '../SelectPickCommand/SelectPickCommand.ts'
