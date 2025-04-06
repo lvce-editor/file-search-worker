9ab975e1557682f25b06cb474cda3aa1f45183c8
Le Vivilet <levivlet@pm.me>
2025-04-06 23:02:45 +0200

    feature: store getPicks functions in an array

--- packages/file-search-worker/src/parts/QuickPickEntryId/QuickPickEntryId.ts
+++ packages/file-search-worker/src/parts/QuickPickEntryId/QuickPickEntryId.ts
@@ -1,25 +1,25 @@
-export const CommandPalette = 0
+export const ColorTheme = 0
 
-export const Commands = 1
+export const CommandPalette = 1
 
-export const File = 2
+export const Commands = 2
 
-export const EveryThing = 3
+export const Custom = 3
 
-export const Recent = 4
+export const EveryThing = 4
 
-export const ColorTheme = 5
+export const File = 5
 
-export const Symbol = 6
+export const GoToLine = 6
 
-export const View = 7
+export const Help = 7
 
-export const Help = 8
+export const Recent = 8
 
-export const WorkspaceSymbol = 9
+export const Symbol = 9
 
-export const Custom = 10
+export const View = 10
 
-export const GoToLine = 11
+export const WorkspaceSymbol = 11
 
 export const Noop = 12
--- packages/file-search-worker/src/parts/SetValue/SetValue.ts
+++ packages/file-search-worker/src/parts/SetValue/SetValue.ts
@@ -3,6 +3,8 @@ import * as FilterQuickPickItems from '../FilterQuickPickItems/FilterQuickPickIt
 import * as GetFilterValue from '../GetFilterValue/GetFilterValue.ts'
 import * as GetPicks from '../GetPicks/GetPicks.ts'
 import * as GetQuickPickFileIcons from '../GetQuickPickFileIcons/GetQuickPickFileIcons.ts'
+import * as GetQuickPickPrefix from '../GetQuickPickPrefix/GetQuickPickPrefix.ts'
+import * as GetQuickPickSubProviderId from '../GetQuickPickSubProviderId/GetQuickPickSubProviderId.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 
 // TODO when user types letters -> no need to query provider again -> just filter existing results
@@ -11,7 +13,9 @@ export const setValue = async (state: QuickPickState, newValue: string): Promise
   if (value === newValue) {
     return state
   }
-  const newPicks = await GetPicks.getPicks(providerId, newValue)
+  const prefix = GetQuickPickPrefix.getQuickPickPrefix(newValue)
+  const subId = GetQuickPickSubProviderId.getQuickPickSubProviderId(prefix)
+  const newPicks = await GetPicks.getPicks(subId, newValue)
   const filterValue = GetFilterValue.getFilterValue(providerId, newValue)
   const items = FilterQuickPickItems.filterQuickPickItems(newPicks, filterValue)
   const focusedIndex = items.length === 0 ? -1 : 0
