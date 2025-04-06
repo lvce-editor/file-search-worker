d996373b86432305c5cf94bd95ec2da823a37fb3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-06 22:08:18 +0200

    feature: remove unused file (#558)

--- packages/file-search-worker/src/parts/QuickPickEntriesModule/QuickPickEntriesModule.ts
+++
@@ -1,7 +0,0 @@
-import type { Dirent } from '../Dirent/Dirent.ts'
-import type { ProtoVisibleItem } from '../ProtoVisibleItem/ProtoVisibleItem.ts'
-
-export interface QuickPickEntriesModule {
-  readonly state: any
-  readonly getPickFileIcon?: (item: ProtoVisibleItem) => Dirent
-}
