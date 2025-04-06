648c286de0c5691c2a65e09e3e5c4ba4c50a3a3b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-06 13:32:41 +0200

    feature: add types for selectPick (#500)

--- packages/file-search-worker/src/parts/SelectPickRecent/SelectPickRecent.ts
+++ packages/file-search-worker/src/parts/SelectPickRecent/SelectPickRecent.ts
@@ -1,9 +1,10 @@
 import type { ProtoVisibleItem } from '../ProtoVisibleItem/ProtoVisibleItem.ts'
+import type { SelectPickResult } from '../SelectPickRresult/SelectPickResult.ts'
 import * as OpenWorkspaceFolder from '../OpenWorkspaceFolder/OpenWorkspaceFolder.ts'
 import * as QuickPickReturnValue from '../QuickPickReturnValue/QuickPickReturnValue.ts'
 
 // TODO selectPick should be independent of show/hide
-export const selectPick = async (pick: ProtoVisibleItem): Promise<any> => {
+export const selectPick = async (pick: ProtoVisibleItem): Promise<SelectPickResult> => {
   const path = `${pick.description}/${pick.label}`
   await OpenWorkspaceFolder.openWorkspaceFolder(path)
   return {
--- packages/file-search-worker/src/parts/SelectPickView/SelectPickView.ts
+++ packages/file-search-worker/src/parts/SelectPickView/SelectPickView.ts
@@ -1,8 +1,10 @@
 import type { ProtoVisibleItem } from '../ProtoVisibleItem/ProtoVisibleItem.ts'
+import type { SelectPickResult } from '../SelectPickRresult/SelectPickResult.ts'
+import * as QuickPickReturnValue from '../QuickPickReturnValue/QuickPickReturnValue.ts'
 
-export const selectPick = async (item: ProtoVisibleItem): Promise<any> => {
+export const selectPick = async (item: ProtoVisibleItem): Promise<SelectPickResult> => {
   // Command.execute(/* openView */ 549, /* viewName */ item.label)
-  // return {
-  //   command: QuickPickReturnValue.Hide,
-  // }
+  return {
+    command: QuickPickReturnValue.Hide,
+  }
 }
