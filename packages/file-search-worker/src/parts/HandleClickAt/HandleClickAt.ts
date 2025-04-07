03802bc436806f063fbdeefc78d5e51b209883ee
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-07 20:23:34 +0200

    feature: move index into separate file (#568)

--- packages/file-search-worker/src/parts/HandleClickAt/HandleClickAt.ts
+++ packages/file-search-worker/src/parts/HandleClickAt/HandleClickAt.ts
@@ -1,9 +1,9 @@
 import type { QuickPickState } from '../QuickPickState/QuickPickState.ts'
+import * as GetIndex from '../GetIndex/GetIndex.ts'
 import { selectIndex } from '../SelectIndex/SelectIndex.ts'
 
 export const handleClickAt = (state: QuickPickState, x: number, y: number): Promise<QuickPickState> => {
   const { top, headerHeight, itemHeight } = state
-  const relativeY = y - top - headerHeight
-  const index = Math.floor(relativeY / itemHeight)
+  const index = GetIndex.getIndex(top, headerHeight, itemHeight, y)
   return selectIndex(state, index)
 }
