a543a1db958baf7ce988aeb111bdf73dc9e544dd
Le Vivilet <levivlet@pm.me>
2025-05-02 18:52:25 +0200

    lint

--- packages/file-search-worker/src/parts/QuickPickStates/QuickPickStates.ts
+++ packages/file-search-worker/src/parts/QuickPickStates/QuickPickStates.ts
@@ -1,4 +1,4 @@
-import type { QuickPickState } from '../QuickPickState/QuickPickState.ts'
 import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
+import type { QuickPickState } from '../QuickPickState/QuickPickState.ts'
 
 export const { get, set, dispose, wrapCommand } = ViewletRegistry.create<QuickPickState>()
