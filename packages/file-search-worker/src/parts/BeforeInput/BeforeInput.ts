261b6cd51fae71ab894894e48dc3eff75bfd1e99
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-30 12:16:06 +0200

    feature: simplify input handler (#655)

--- packages/file-search-worker/src/parts/BeforeInput/BeforeInput.ts
+++ packages/file-search-worker/src/parts/BeforeInput/BeforeInput.ts
@@ -1,11 +1,7 @@
+import type { InputResult } from '../InputResult/InputResult.ts'
 import * as GetNewValueFunction from '../GetNewValueFunction/GetNewValueFunction.ts'
 
-interface Result {
-  readonly newValue: string
-  readonly cursorOffset: number
-}
-
-export const getNewValue = (value: string, inputType: string, data: string, selectionStart: number, selectionEnd: number): Result => {
+export const getNewValue = (value: string, inputType: string, data: string, selectionStart: number, selectionEnd: number): InputResult => {
   const fn = GetNewValueFunction.getNewValueFunction(inputType)
   return fn(value, selectionStart, selectionEnd, data)
 }
