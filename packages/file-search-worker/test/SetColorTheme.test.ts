3e431c8cc02bbc96e185f62f3aed51c8f8092ac7
Le Vivilet <levivlet@pm.me>
2025-06-30 13:11:58 +0200

    lint

--- packages/file-search-worker/test/SetColorTheme.test.ts
+++ packages/file-search-worker/test/SetColorTheme.test.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@jest/globals'
-import { setColorTheme } from '../src/parts/SetColorTheme/SetColorTheme.js'
 import { MockRpc } from '@lvce-editor/rpc'
-import { set } from '../src/parts/RpcRegistry/RpcRegistry.ts'
 import { RendererWorker } from '../src/parts/RpcId/RpcId.ts'
+import { set } from '../src/parts/RpcRegistry/RpcRegistry.ts'
+import { setColorTheme } from '../src/parts/SetColorTheme/SetColorTheme.js'
 
 test('setColorTheme should invoke ColorTheme.setColorTheme with the provided id', async () => {
   let invokedMethod: string | undefined
