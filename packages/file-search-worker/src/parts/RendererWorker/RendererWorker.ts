71f7f11dfe13fe2e48285329535ec758852e654d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-04 10:34:40 +0200

    feature: simplify renderer worker api (#671)

--- packages/file-search-worker/src/parts/RendererWorker/RendererWorker.ts
+++ packages/file-search-worker/src/parts/RendererWorker/RendererWorker.ts
@@ -1,3 +1,3 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const { invoke, set, invokeAndTransfer } = RendererWorker
+export const { invoke, set, invokeAndTransfer, activateByEvent } = RendererWorker
