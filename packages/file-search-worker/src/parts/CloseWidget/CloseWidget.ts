b3b2596b26d088e5984b7db2ed54ed453d56daae
Le Vivilet <levivlet@pm.me>
2025-07-04 10:27:18 +0200

    feature: rename module

--- packages/file-search-worker/src/parts/CloseWidget/CloseWidget.ts
+++ packages/file-search-worker/src/parts/CloseWidget/CloseWidget.ts
@@ -1,5 +1,5 @@
-import * as ParentRpc from '../RendererWorker/RendererWorker.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const closeWidget = async (id: number): Promise<void> => {
-  await ParentRpc.invoke('Viewlet.closeWidget', id)
+  await RendererWorker.invoke('Viewlet.closeWidget', id)
 }
--- packages/file-search-worker/src/parts/Listen/Listen.ts
+++ packages/file-search-worker/src/parts/Listen/Listen.ts
@@ -1,6 +1,6 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import * as ParentRpc from '../RendererWorker/RendererWorker.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import * as SearchFileModule from '../SearchFileModule/SearchFileModule.ts'
 import * as SearchModules from '../SearchModules/SearchModules.ts'
 
@@ -9,5 +9,5 @@ export const listen = async (): Promise<void> => {
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
-  ParentRpc.set(rpc)
+  RendererWorker.set(rpc)
 }
