0393038389ff7646d5284adf4d88fe091c98e8e6
Le Vivilet <levivilet@pm.me>
2025-10-31 20:50:19 +0100

    feature: update recent pick label

--- packages/file-search-worker/src/parts/GetPicksOpenRecent/GetPicksOpenRecent.ts
+++ packages/file-search-worker/src/parts/GetPicksOpenRecent/GetPicksOpenRecent.ts
@@ -2,15 +2,22 @@ import type { ProtoVisibleItem } from '../ProtoVisibleItem/ProtoVisibleItem.ts'
 import * as DirentType from '../DirentType/DirentType.ts'
 import * as GetRecentlyOpened from '../GetRecentlyOpened/GetRecentlyOpened.ts'
 
+const getLabel = (uri: string): string => {
+  if (uri.startsWith('file://')) {
+    return uri.slice('file://'.length)
+  }
+  return uri
+}
+
 const toProtoVisibleItem = (uri: string): ProtoVisibleItem => {
   return {
-    label: uri,
+    label: getLabel(uri),
     description: '',
     fileIcon: '',
     icon: '',
     matches: [],
     direntType: DirentType.Directory,
-    uri: uri,
+    uri,
   }
 }
 
