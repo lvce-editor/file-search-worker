1eba22a538bbb166089adfc0082d64d1ec4271ae
Le Vivilet <levivilet@pm.me>
2025-11-16 20:56:18 +0100

    feature: hide when checking for updates

--- packages/file-search-worker/src/parts/ShouldHide/ShouldHide.ts
+++ packages/file-search-worker/src/parts/ShouldHide/ShouldHide.ts
@@ -1,5 +1,7 @@
+const hideIds = ['AutoUpdater.checkForUpdates']
+
 export const shouldHide = (item: any): boolean => {
-  if (item.id === 'Viewlet.openWidget' && item.args[0] === 'QuickPick') {
+  if (hideIds.includes(item.id) || (item.id === 'Viewlet.openWidget' && item.args[0] === 'QuickPick')) {
     return false
   }
   return true
