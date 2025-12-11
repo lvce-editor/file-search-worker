27c16b4a6e759ca4d6349010ef60966feff2abcb
Le Vivilet <levivilet@pm.me>
2025-12-11 18:42:31 +0100

    e2e

--- packages/e2e/src/quickpick.handle-wheel.ts
+++ packages/e2e/src/quickpick.handle-wheel.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'quickpick.handle-wheel'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, expect, Locator, QuickPick }) => {
   // arrange
   await QuickPick.open()
