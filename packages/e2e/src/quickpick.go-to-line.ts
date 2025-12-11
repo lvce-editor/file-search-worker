db4156930d23e627d97574d79b34804061de11be
Le Vivilet <levivilet@pm.me>
2025-12-11 17:14:06 +0100

    feature: add go to line e2e test

---
+++ packages/e2e/src/quickpick.go-to-line.ts
@@ -0,0 +1,20 @@
+import { type Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'quickpick.go-to-line'
+
+export const skip = 1
+
+export const test: Test = async ({ expect, FileSystem, Locator, Main, QuickPick, SideBar, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/a.txt`, 'abc')
+  await Workspace.setPath(tmpDir)
+  await QuickPick.open() // TODO open with go to line
+
+  // act
+  // TODO enter a line number and select it
+
+  // assert
+
+  // TODO verify the expected editor position
+}
