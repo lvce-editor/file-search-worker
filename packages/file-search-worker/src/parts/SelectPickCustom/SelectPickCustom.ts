8359fbc13789442246880e0136945fbe745bf078
Le Vivilet <levivilet@pm.me>
2025-12-11 19:14:10 +0100

    feature: when selecting custom quickpick item, send result to renderer worker

--- packages/file-search-worker/src/parts/SelectPickCustom/SelectPickCustom.ts
+++ packages/file-search-worker/src/parts/SelectPickCustom/SelectPickCustom.ts
@@ -2,11 +2,12 @@ import type { ProtoVisibleItem } from '../ProtoVisibleItem/ProtoVisibleItem.ts'
 import type { SelectPickResult } from '../SelectPickRresult/SelectPickResult.ts'
 import { state } from '../QuickPickEntriesCustom/QuickPickEntriesCustomState.ts'
 import * as QuickPickReturnValue from '../QuickPickReturnValue/QuickPickReturnValue.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const selectPick = async (pick: ProtoVisibleItem): Promise<SelectPickResult> => {
   const { args } = state
-  const resolve = args[2]
-  resolve(pick)
+  const resolveId = args[2]
+  await RendererWorker.invoke(`QuickPick.executeCallback`, resolveId, pick)
   return {
     command: QuickPickReturnValue.Hide,
   }
