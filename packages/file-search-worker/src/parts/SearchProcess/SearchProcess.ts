be978362b77ca469a43ebca00322841c6744050e
Le Vivilet <levivilet@pm.me>
2025-12-12 19:31:57 +0100

    tsc

--- packages/file-search-worker/src/parts/SearchProcess/SearchProcess.ts
+++ packages/file-search-worker/src/parts/SearchProcess/SearchProcess.ts
@@ -1,5 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const invoke = (method: string, ...params: readonly unknown[]): Promise<unknown> => {
+export const invoke = (method: string, ...params: readonly unknown[]): Promise<any> => {
   return RendererWorker.invoke('SearchProcess.invoke', method, ...params)
 }
