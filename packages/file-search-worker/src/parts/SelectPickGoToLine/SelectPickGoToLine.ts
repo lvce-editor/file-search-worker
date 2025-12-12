7216fc988ab2e0f83414e9bb17537db6568d2aa6
Le Vivilet <levivilet@pm.me>
2025-12-12 23:56:25 +0100

    feature: show message for gotoline picks (#766)
    * feature: show message for gotoline picks
    
    * update variable

--- packages/file-search-worker/src/parts/SelectPickGoToLine/SelectPickGoToLine.ts
+++ packages/file-search-worker/src/parts/SelectPickGoToLine/SelectPickGoToLine.ts
@@ -1,11 +1,19 @@
 import type { SelectPickResult } from '../SelectPickRresult/SelectPickResult.ts'
 import { goToPositionAndFocus } from '../GoToPositionAndFocus/GoToPositionAndFocus.ts'
+import * as QuickPickPrefix from '../QuickPickPrefix/QuickPickPrefix.ts'
 import * as QuickPickReturnValue from '../QuickPickReturnValue/QuickPickReturnValue.ts'
 
 export const selectPick = async (item: any, value: string): Promise<SelectPickResult> => {
-  const rowIndex = Number.parseInt(item.label)
-  const columnIndex = 0
-  await goToPositionAndFocus(rowIndex, columnIndex)
+  if (value.startsWith(QuickPickPrefix.GoToLine)) {
+    const lineString = value.slice(QuickPickPrefix.GoToLine.length)
+    const wantedLine = Number.parseInt(lineString, 10)
+    const rowIndex = wantedLine - 1
+    const columnIndex = 0
+    await goToPositionAndFocus(rowIndex, columnIndex)
+    return {
+      command: QuickPickReturnValue.Hide,
+    }
+  }
   return {
     command: QuickPickReturnValue.Hide,
   }
