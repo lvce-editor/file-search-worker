22686b9eb68ab7bd3432d63c82882e15265bbdf1
Le Vivilet <levivilet@pm.me>
2025-12-12 10:25:10 +0100

    feature: implement go to line

--- packages/file-search-worker/src/parts/SelectPickGoToLine/SelectPickGoToLine.ts
+++ packages/file-search-worker/src/parts/SelectPickGoToLine/SelectPickGoToLine.ts
@@ -1,15 +1,11 @@
 import type { SelectPickResult } from '../SelectPickRresult/SelectPickResult.ts'
-import * as Command from '../Command/Command.ts'
 import * as QuickPickReturnValue from '../QuickPickReturnValue/QuickPickReturnValue.ts'
+import { setCursor } from '../SetCursor/SetCursor.ts'
 
 export const selectPick = async (item: any): Promise<SelectPickResult> => {
   const rowIndex = Number.parseInt(item.label)
-  const position = {
-    columnIndex: 5,
-    rowIndex,
-  }
-  await Command.execute(/* EditorSetCursor.editorSetCursor */ 'TODO', /* position */ position)
-  // TODO put cursor onto that line
+  const columnIndex = 5
+  await setCursor(rowIndex, columnIndex)
   return {
     command: QuickPickReturnValue.Hide,
   }
