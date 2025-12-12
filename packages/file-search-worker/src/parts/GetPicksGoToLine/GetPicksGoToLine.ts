27f229588db5a0677bc5195c3386fe38d4926580
Le Vivilet <levivilet@pm.me>
2025-12-12 22:53:16 +0100

    feature: for invalid column, return message

--- packages/file-search-worker/src/parts/GetPicksGoToLine/GetPicksGoToLine.ts
+++ packages/file-search-worker/src/parts/GetPicksGoToLine/GetPicksGoToLine.ts
@@ -4,24 +4,31 @@ import { getPosition } from '../GetPosition/GetPosition.ts'
 import { getText } from '../GetText/GetText.ts'
 import * as QuickPickPrefix from '../QuickPickPrefix/QuickPickPrefix.ts'
 
+const getPicksGoToColumnBase = async (): Promise<readonly ProtoVisibleItem[]> => {
+  const text = await getText()
+  return [
+    {
+      description: '',
+      direntType: 0,
+      fileIcon: '',
+      icon: '',
+      label: `Type a character position to go to (from 1 to ${text.length})`,
+      matches: [],
+      uri: '',
+    },
+  ]
+}
+
 export const getPicks = async (value: string): Promise<readonly ProtoVisibleItem[]> => {
   if (value === QuickPickPrefix.GoToColumn) {
-    const text = await getText()
-    return [
-      {
-        description: '',
-        direntType: 0,
-        fileIcon: '',
-        icon: '',
-        label: `Type a character position to go to (from 1 to ${text.length})`,
-        matches: [],
-        uri: '',
-      },
-    ]
+    return getPicksGoToColumnBase()
   }
   if (value.startsWith(QuickPickPrefix.GoToColumn)) {
     const columnString = value.slice(2)
     const wantedColumn = Number.parseInt(columnString, 10)
+    if (Number.isNaN(wantedColumn)) {
+      return getPicksGoToColumnBase()
+    }
     const text = await getText()
     const position = getPosition(text, wantedColumn)
     return [
