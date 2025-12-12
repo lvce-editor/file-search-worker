7c6108cdd8734e55d6fae7463ad610b719696edc
Le Vivilet <levivilet@pm.me>
2025-12-12 13:44:00 +0100

    feature: add column e2e test (#722)

---
+++ packages/e2e/src/quickpick.go-to-column.ts
@@ -0,0 +1,21 @@
+import { type Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'quickpick.go-to-column'
+
+export const skip = 1
+
+export const test: Test = async ({ Editor, FileSystem, Main, QuickPick, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/a.txt`, 'abc\ndef')
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/a.txt`)
+  await QuickPick.open()
+  await QuickPick.setValue('::2')
+
+  // act
+  await QuickPick.selectItem('2')
+
+  // assert
+  await Editor.shouldHaveSelections(new Uint32Array([0, 2, 0, 2]))
+}
