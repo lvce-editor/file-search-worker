b55f475b00f1aab215405dd3e1b06cbe1bc1eedb
Le Vivilet <levivilet@pm.me>
2025-12-12 19:10:24 +0100

    feature: simplify command ids (#738)

--- packages/file-search-worker/src/parts/Listen/Listen.ts
+++ packages/file-search-worker/src/parts/Listen/Listen.ts
@@ -1,10 +1,12 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import { registerCommands } from '../QuickPickStates/QuickPickStates.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import * as SearchFileModule from '../SearchFileModule/SearchFileModule.ts'
 import * as SearchModules from '../SearchModules/SearchModules.ts'
 
 export const listen = async (): Promise<void> => {
+  registerCommands(CommandMap.commandMap)
   SearchFileModule.register(SearchModules.searchModules)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
