820fd0a84e64fc5126a8052888e37b8083584c45
Le Vivilet <levivilet@pm.me>
2026-01-07 16:32:25 +0100

    feature: remove unused comment (#772)

--- packages/file-search-worker/src/parts/GetColorThemeNames/GetColorThemeNames.ts
+++ packages/file-search-worker/src/parts/GetColorThemeNames/GetColorThemeNames.ts
@@ -1,5 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const getColorThemeNames = async (): Promise<readonly string[]> => {
-  return RendererWorker.invoke(/* Ajax.getJson */ 'ColorTheme.getColorThemeNames')
+  return RendererWorker.invoke('ColorTheme.getColorThemeNames')
 }
