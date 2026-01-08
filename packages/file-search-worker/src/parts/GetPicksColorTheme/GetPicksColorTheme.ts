972efd89ac5473d2fdef5f5c1611f52e5188aa2b
Le Vivilet <levivilet@pm.me>
2026-01-08 11:36:07 +0100

    fix

--- packages/file-search-worker/src/parts/GetPicksColorTheme/GetPicksColorTheme.ts
+++ packages/file-search-worker/src/parts/GetPicksColorTheme/GetPicksColorTheme.ts
@@ -14,12 +14,7 @@ const toProtoVisibleItem = (name: string): ProtoVisibleItem => {
   return pick
 }
 
-export const getPicks = async (searchValue: string, args?: any): Promise<readonly ProtoVisibleItem[]> => {
-  if (!args || !Array.isArray(args)) {
-    args = ['', 0]
-  }
-  const platform = args.at(-1)
-  const assetDir = args.at(-2)
+export const getPicks = async (searchValue: string, args?: any, { assetDir = '', platform = 0 } = {}): Promise<readonly ProtoVisibleItem[]> => {
   const colorThemeNames = await GetColorThemeNames.getColorThemeNames(assetDir, platform)
   const picks = colorThemeNames.map(toProtoVisibleItem)
   return picks
