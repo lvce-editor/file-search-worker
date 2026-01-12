1c4910880aaa65ebce6454fd24a869cb0ef64408
Le Vivilet <levivilet@pm.me>
2026-01-12 19:54:24 +0100

    feature: improve messageport rpc

--- packages/file-search-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
+++ packages/file-search-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -1,9 +1,10 @@
-import { MessagePortRpcClient } from '@lvce-editor/rpc'
+import { PlainMessagePortRpc } from '@lvce-editor/rpc'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 
 export const handleMessagePort = async (port: MessagePort): Promise<void> => {
-  await MessagePortRpcClient.create({
+  await PlainMessagePortRpc.create({
     commandMap: CommandMapRef.commandMapRef,
+    isMessagePortOpen: true,
     messagePort: port,
   })
 }
