7dd097fcdb565c08f6a5b77d02ae1f4b1c4ed10f
Le Vivilet <levivilet@pm.me>
2026-01-12 20:10:57 +0100

    feature: ask renderer worker to show quickpick

--- packages/file-search-worker/src/parts/ShowQuickInput/ShowQuickInput.ts
+++ packages/file-search-worker/src/parts/ShowQuickInput/ShowQuickInput.ts
@@ -1,8 +1,11 @@
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { QuickInputOptions } from '../QuickInputOptions/QuickInputOptions.ts'
 import type { QuickInputResult } from '../QuickInputResult/QuickInputResult.ts'
 
 export const showQuickInput = async ({ ignoreFocusOut, initialValue }: QuickInputOptions): Promise<QuickInputResult> => {
   // TODO ask renderer worker to create quickpick instance, with given options
+  const picks: readonly any[] = []
+  await RendererWorker.invoke('QuickPick.showCustom', picks, { ignoreFocusOut, initialValue })
   return {
     canceled: false,
     inputValue: '',
