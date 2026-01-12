ce5c87f2fc97ea38e97c89081bd7ca4d13340859
Le Vivilet <levivilet@pm.me>
2026-01-12 17:19:42 +0100

    fix

--- packages/file-search-worker/src/parts/GetPicks/GetPicks.ts
+++ packages/file-search-worker/src/parts/GetPicks/GetPicks.ts
@@ -5,17 +5,12 @@ export interface IGetPicks {
   (value: string, args: readonly unknown[], { assetDir, platform }: { assetDir: string; platform: number }): Promise<readonly ProtoVisibleItem[]>
 }
 
-<<<<<<< HEAD
-export const getPicks = (id: number, searchValue: string, args: readonly unknown[]): Promise<readonly ProtoVisibleItem[]> => {
-  console.log({ id, searchValue })
-=======
 export const getPicks = (
   id: number,
   searchValue: string,
   args: readonly unknown[],
   { assetDir, platform }: { assetDir: string; platform: number },
 ): Promise<readonly ProtoVisibleItem[]> => {
->>>>>>> origin/main
   const fn = QuickPickEntries.getPicks(id)
   return fn(searchValue, args, { assetDir, platform })
 }
