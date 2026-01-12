be24165f3ad92647d691f91fdd4716e60276b908
Le Vivilet <levivilet@pm.me>
2026-01-12 17:40:42 +0100

    feature: update types (#780)

--- packages/file-search-worker/src/parts/ShowQuickInput/ShowQuickInput.ts
+++ packages/file-search-worker/src/parts/ShowQuickInput/ShowQuickInput.ts
@@ -1,5 +1,10 @@
 import type { QuickInputOptions } from '../QuickInputOptions/QuickInputOptions.ts'
+import type { QuickInputResult } from '../QuickInputResult/QuickInputResult.ts'
 
-export const showQuickInput = async ({ ignoreFocusOut, initialValue }: QuickInputOptions): Promise<void> => {
+export const showQuickInput = async ({ ignoreFocusOut, initialValue }: QuickInputOptions): Promise<QuickInputResult> => {
   // TODO ask renderer worker to create quickpick instance, with given options
+  return {
+    canceled: false,
+    inputValue: '',
+  }
 }
