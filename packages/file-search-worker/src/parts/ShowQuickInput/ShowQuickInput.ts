b43558e8c00b442e308dc74b7c264c3178a15a0b
Le Vivilet <levivilet@pm.me>
2026-01-12 17:18:38 +0100

    move

--- packages/file-search-worker/src/parts/ShowQuickInput/ShowQuickInput.ts
+++ packages/file-search-worker/src/parts/ShowQuickInput/ShowQuickInput.ts
@@ -1,8 +1,4 @@
-interface QuickInputOptions {
-  readonly ignoreFocusOut?: boolean
-  readonly initialValue?: string
-  readonly render?: any
-}
+import type { QuickInputOptions } from '../QuickInputOptions/QuickInputOptions.ts'
 
 export const showQuickInput = async ({ ignoreFocusOut, initialValue }: QuickInputOptions): Promise<void> => {
   // TODO ask renderer worker to create quickpick instance, with given options
