21b9fb14c9c140089e7718d99e9cef9ebff35338
Le Vivilet <levivilet@pm.me>
2026-01-13 20:50:21 +0100

    fix: include hidden files in search

--- packages/file-search-worker/src/parts/GetFileSearchRipGrepArgs/GetFileSearchRipGrepArgs.ts
+++ packages/file-search-worker/src/parts/GetFileSearchRipGrepArgs/GetFileSearchRipGrepArgs.ts
@@ -1,4 +1,4 @@
 export const getFileSearchRipGrepArgs = (): readonly string[] => {
-  const ripGrepArgs = ['--files', '--sort-files']
+  const ripGrepArgs = ['--files', '--sort-files', '--hidden']
   return ripGrepArgs
 }
