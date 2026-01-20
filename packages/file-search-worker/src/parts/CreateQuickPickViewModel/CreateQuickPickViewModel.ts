816367a3772638c6c36fd50deac918d3ace51298
Le Vivilet <levivilet@pm.me>
2026-01-20 20:40:57 +0100

    feature: use destructuring (#799)

--- packages/file-search-worker/src/parts/CreateQuickPickViewModel/CreateQuickPickViewModel.ts
+++ packages/file-search-worker/src/parts/CreateQuickPickViewModel/CreateQuickPickViewModel.ts
@@ -7,27 +7,43 @@ import * as GetVisibleQuickPickItems from '../GetVisibleQuickPickItems/GetVisibl
 import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
 
 export const createQuickPickViewModel = (oldState: QuickPickState, newState: QuickPickState): QuickPickViewModel => {
-  const protoVisibleItems = GetProtoVisibleQuickPickItems.getVisible(newState.items, newState.minLineY, newState.maxLineY, newState.icons)
-  const visibleItems = GetVisibleQuickPickItems.getVisible(newState.items.length, protoVisibleItems, newState.minLineY, newState.focusedIndex)
+  const {
+    cursorOffset,
+    deltaY,
+    finalDeltaY,
+    focused,
+    focusedIndex,
+    headerHeight,
+    height,
+    icons,
+    itemHeight,
+    items,
+    maxLineY,
+    minimumSliderSize,
+    minLineY,
+    uid,
+    value,
+  } = newState
+  const protoVisibleItems = GetProtoVisibleQuickPickItems.getVisible(items, minLineY, maxLineY, icons)
+  const visibleItems = GetVisibleQuickPickItems.getVisible(items.length, protoVisibleItems, minLineY, focusedIndex)
   const oldFocusedIndex = oldState.focusedIndex - oldState.minLineY
-  const newFocusedIndex = newState.focusedIndex - newState.minLineY
-  const itemCount = newState.items.length
-  const { deltaY, finalDeltaY, headerHeight, height, itemHeight, minimumSliderSize } = newState
+  const newFocusedIndex = focusedIndex - minLineY
+  const itemCount = items.length
   const listHeight = GetListHeight.getListHeight(itemCount, itemHeight, height)
   const contentHeight = itemCount * itemHeight
   const scrollBarHeight = GetScrollBarSize.getScrollBarSize(listHeight, contentHeight, minimumSliderSize)
   const scrollBarY = ScrollBarFunctions.getScrollBarY(deltaY, finalDeltaY, height - headerHeight, scrollBarHeight)
   const roundedScrollBarY = Math.round(scrollBarY)
   return {
-    cursorOffset: newState.cursorOffset,
-    focused: newState.focused,
+    cursorOffset,
+    focused,
     height,
     newFocusedIndex,
     oldFocusedIndex,
     scrollBarHeight,
     scrollBarTop: roundedScrollBarY,
-    uid: newState.uid,
-    value: newState.value,
+    uid,
+    value,
     visibleItems,
   }
 }
