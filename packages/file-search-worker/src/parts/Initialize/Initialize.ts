9287c71baa1baf7fa821afa64c1e7ff0fc2a9eb8
Le Vivilet <levivilet@pm.me>
2026-01-20 20:50:51 +0100

    feature: simplify initializing editor worker rpc (#802)

--- packages/file-search-worker/src/parts/Initialize/Initialize.ts
+++ packages/file-search-worker/src/parts/Initialize/Initialize.ts
@@ -1,13 +1,3 @@
-import { TransferMessagePortRpcParent } from '@lvce-editor/rpc'
-import { EditorWorker, RendererWorker } from '@lvce-editor/rpc-registry'
-
 export const initialize = async (): Promise<void> => {
-  // TODO
-  const rpc = await TransferMessagePortRpcParent.create({
-    commandMap: {},
-    async send(port) {
-      await RendererWorker.sendMessagePortToEditorWorker(port, 0)
-    },
-  })
-  EditorWorker.set(rpc)
+  // not needed anymore
 }
