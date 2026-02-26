c1163ddc66b26d3a36f163202b6ec82165845b39
Le Vivilet <levivilet@pm.me>
2026-02-26 10:17:53 +0100

    import

--- packages/file-search-worker/src/parts/DiffCss/DiffCss.ts
+++ packages/file-search-worker/src/parts/DiffCss/DiffCss.ts
@@ -1,5 +1,4 @@
 import type { QuickPickState } from '../QuickPickState/QuickPickState.ts'
-import * as DiffType from '../DiffType/DiffType.ts'
 
 export const isEqual = (oldState: QuickPickState, newState: QuickPickState): boolean => {
   return oldState.items.length === newState.items.length && oldState.itemHeight === newState.itemHeight
