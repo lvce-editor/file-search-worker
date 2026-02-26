1a837d67f419ed2706807c7b9e3c134a2ae8c34c
Le Vivilet <levivilet@pm.me>
2026-02-26 10:14:53 +0100

    feature: update DiffModules to include DiffType and modify numbers export

--- packages/file-search-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/file-search-worker/src/parts/DiffModules/DiffModules.ts
@@ -2,8 +2,9 @@ import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffFocusedIndex from '../DiffFocusedIndex/DiffFocusedIndex.ts'
 import * as DiffHeight from '../DiffHeight/DiffHeight.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
+import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
 export const modules = [DiffHeight.isEqual, DiffItems.isEqual, DiffValue.isEqual, DiffFocusedIndex.isEqual, DiffFocus.isEqual]
 
-export const numbers = [DiffHeight.diffType, DiffItems.diffType, DiffValue.diffType, DiffFocusedIndex.diffType, DiffFocus.diffType]
+export const numbers = [DiffType.Height, DiffType.RenderItems, DiffValue.diffType, DiffType.RenderFocusedIndex, DiffType.RenderFocus]
