911fba2734b19d41b0b9644fb3c5b38d273771f7
Le Vivilet <levivilet@pm.me>
2026-03-23 18:06:04 +0100

    feature: add initial state handling to QuickPickState and related components

--- packages/file-search-worker/src/parts/RenderIncremental/RenderIncremental.ts
+++ packages/file-search-worker/src/parts/RenderIncremental/RenderIncremental.ts
@@ -6,6 +6,9 @@ import { getItemsDom } from '../RenderItems/RenderItems.ts'
 export const renderIncremental = (oldState: QuickPickState, newState: QuickPickState): readonly unknown[] => {
   const oldDom = getItemsDom(oldState) as any
   const newDom = getItemsDom(newState) as any
+  if (oldState.initial) {
+    return [ViewletCommand.SetDom2, newState.uid, newDom]
+  }
   const patches = diffTree(oldDom, newDom)
   return [ViewletCommand.SetPatches, newState.uid, patches]
 }
