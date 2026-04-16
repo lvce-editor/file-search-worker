db0ee3521f27524a2c3164cc94d379fb471af454
Le Vivilet <levivilet@pm.me>
2026-04-16 13:20:01 +0200

    fix: correct typo in FileSystemProtocol import and usage (#837)

--- packages/file-search-worker/src/parts/SearchModules/SearchModules.ts
+++ packages/file-search-worker/src/parts/SearchModules/SearchModules.ts
@@ -1,5 +1,4 @@
 import type { SearchFileHandler } from '../SearchFileHandler/SearchFileHandler.ts'
-import * as FileSystemProcotol from '../FileSystemProtocol/FileSystemProtocol.ts'
 import * as FileSystemProtocol from '../FileSystemProtocol/FileSystemProtocol.ts'
 import * as SearchFileMemfs from '../SearchFileMemfs/SearchFileMemfs.ts'
 import * as SearchFileFetch from '../SearchFileWithFetch/SearchFileWithFetch.ts'
@@ -7,9 +6,9 @@ import * as SearchFileHtml from '../SearchFileWithHtml/SearchFileWithHtml.ts'
 import * as SearchFileRipGrep from '../SearchFileWithRipGrep/SearchFileWithRipGrep.ts'
 
 export const searchModules: Record<string, SearchFileHandler> = {
-  [FileSystemProcotol.Default]: SearchFileRipGrep.searchFile,
-  [FileSystemProcotol.Fetch]: SearchFileFetch.searchFile,
-  [FileSystemProcotol.File]: SearchFileRipGrep.searchFile,
+  [FileSystemProtocol.Default]: SearchFileRipGrep.searchFile,
+  [FileSystemProtocol.Fetch]: SearchFileFetch.searchFile,
+  [FileSystemProtocol.File]: SearchFileRipGrep.searchFile,
   [FileSystemProtocol.Html]: SearchFileHtml.searchFile,
   [FileSystemProtocol.Memfs]: SearchFileMemfs.searchFile,
 }
