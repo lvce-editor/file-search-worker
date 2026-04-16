724dacffb093ae88e9b99a1c39e0d57231fa3da2
Le Vivilet <levivilet@pm.me>
2026-04-16 09:05:39 +0200

    feature: add handleMessagePort function and update commandMap

--- packages/file-search-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/file-search-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,8 @@
+import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 import * as SearchFile from '../SearchFile/SearchFile.ts'
 
 export const commandMap = {
+  'FileSearch.handleMessagePort': handleMessagePort,
   'FileSearch.searchFile': SearchFile.searchFile,
   'SearchFile.searchFile': SearchFile.searchFile,
 }
