315b9b3d0330666cea5437ca729c5f65b09e0a1b
Le Vivilet <levivilet@pm.me>
2026-08-05 21:27:45 +0000

    fix: exclude elm-stuff from file search

--- packages/file-search-worker/src/parts/GetFileSearchRipGrepArgs/GetFileSearchRipGrepArgs.ts
+++ packages/file-search-worker/src/parts/GetFileSearchRipGrepArgs/GetFileSearchRipGrepArgs.ts
@@ -1,4 +1,4 @@
 export const getFileSearchRipGrepArgs = (): readonly string[] => {
-  const ripGrepArgs = ['--files', '--sort-files', '--hidden', '--glob', '!.git']
+  const ripGrepArgs = ['--files', '--sort-files', '--hidden', '--glob', '!.git', '--glob', '!elm-stuff']
   return ripGrepArgs
 }
